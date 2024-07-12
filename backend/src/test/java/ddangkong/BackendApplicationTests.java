cb814d456b8fc4b1aa7e488200dae98ce9998c6e
Nam Gi Beom <rlqja0523@naver.com>
2024-07-12 16:48:27 +0900

    init: 초기 세팅 #1

---
+++ backend/src/test/java/ddangkong/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ddangkong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
