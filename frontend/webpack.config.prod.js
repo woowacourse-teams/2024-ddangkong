4355150f23f7881200a086ca1a420719e17243d0
rbgksqkr <rbgks1937@naver.com>
2024-07-13 16:49:08 +0900

    init: prettier 세팅 #2

--- frontend/webpack.config.prod.js
+++ frontend/webpack.config.prod.js
@@ -1,16 +1,16 @@
-const { merge } = require("webpack-merge");
-const common = require("./webpack.config.common.js");
+const { merge } = require('webpack-merge');
+const common = require('./webpack.config.common.js');
 
 module.exports = merge(common, {
-  mode: "production",
-  devtool: "source-map",
+  mode: 'production',
+  devtool: 'source-map',
   module: {
     rules: [
       {
         test: /\.(js|jsx|ts|tsx)$/,
         exclude: /node_modules/,
-        use: ["babel-loader"],
+        use: ['babel-loader'],
       },
-    ]
-  }
+    ],
+  },
 });
