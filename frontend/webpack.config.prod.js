613aa32e94d33829d7ba08acdab75d81cf877557
rbgksqkr <rbgks1937@naver.com>
2024-07-13 16:26:55 +0900

    chore: 개발환경에서 ts-loader, 프로덕션환경에서 babel-loader 적용 #2

--- frontend/webpack.config.prod.js
+++ frontend/webpack.config.prod.js
@@ -4,4 +4,13 @@ const common = require("./webpack.config.common.js");
 module.exports = merge(common, {
   mode: "production",
   devtool: "source-map",
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx|ts|tsx)$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"],
+      },
+    ]
+  }
 });
