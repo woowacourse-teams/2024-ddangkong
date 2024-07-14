c42d839768957592d4c81b2f1d3f0bc881e95ff2
rbgksqkr <rbgks1937@naver.com>
2024-07-14 15:13:13 +0900

    chore: storybook 실행 환경 설정 #2

---
+++ frontend/.storybook/main.ts
@@ -0,0 +1,45 @@
+import type { StorybookConfig } from '@storybook/react-webpack5';
+import path from 'path';
+import { Configuration } from 'webpack';
+
+const config: StorybookConfig = {
+  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+  addons: [
+    '@storybook/addon-webpack5-compiler-swc',
+    '@storybook/addon-onboarding',
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@chromatic-com/storybook',
+    '@storybook/addon-interactions',
+  ],
+  framework: {
+    name: '@storybook/react-webpack5',
+    options: {
+      builder: {
+        useSWC: true,
+      },
+    },
+  },
+  swc: () => ({
+    jsc: {
+      transform: {
+        react: {
+          runtime: 'automatic',
+        },
+      },
+    },
+  }),
+  webpackFinal: async (config: Configuration) => {
+    const { resolve } = config;
+
+    if (resolve) {
+      resolve.alias = {
+        ...resolve.alias,
+        '@': path.resolve(__dirname, '../src'),
+      };
+    }
+
+    return config;
+  },
+};
+export default config;
