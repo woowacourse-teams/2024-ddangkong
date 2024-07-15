9a9b551026b1143a184b4fa64dfde56dd7d66ba6
useon <roqkftjs@gmail.com>
2024-07-16 08:05:56 +0900

    refactor: 파일명 대문자로 수정 #7

---
+++ frontend/src/components/TopicContainer/TopicContainer.styled.ts
@@ -0,0 +1,18 @@
+import { css } from '@emotion/react';
+
+export const topicLayout = css`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 1.4rem;
+`;
+
+export const categoryText = css`
+  font-weight: bold;
+  font-size: 1.4rem;
+`;
+
+export const topicText = css`
+  font-weight: bold;
+  font-size: 2rem;
+`;
