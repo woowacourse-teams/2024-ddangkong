4f63e8e7a04fbaccbcd686c99273f3412b3dd85c
rbgksqkr <rbgks1937@naver.com>
2024-07-16 03:28:28 +0900

    feat: 투표 결과를 반영 post 요청 mocking #6

--- frontend/src/apis/question.ts
+++ frontend/src/apis/question.ts
@@ -2,10 +2,26 @@ const BASE_URL = process.env.NODE_ENV === 'production' ? process.env.API_BASE_UR
 
 const URL = {
   question: (roomId: number) => `${BASE_URL}/api/balances/rooms/${roomId}/question`,
+  vote: (roomId: number, questionId: number) =>
+    `${BASE_URL}/api/balances/rooms/${roomId}/questions/${questionId}/votes`,
 };
 
 export const fetchQuestion = async (roomId = 1) => {
   const res = await fetch(URL.question(roomId));
+
+  const data = await res.json();
+
+  return data;
+};
+
+export const voteQuestion = async (choiceId: number, roomId = 1, questionId = 1) => {
+  const res = await fetch(URL.vote(roomId, questionId), {
+    method: 'POST',
+    body: JSON.stringify({
+      memberId: 1,
+      choiceId,
+    }),
+  });
 
   const data = await res.json();
 
