3f1e26f6bc4b2d87379eec35015fcb7aaafb6aca
Nam Gi Beom <rlqja0523@naver.com>
2024-07-16 10:37:40 +0900

    feat: exception handling 로직 추가 #1

---
+++ backend/src/main/java/ddangkong/controller/exception/GlobalExceptionHandler.java
@@ -0,0 +1,37 @@
+package ddangkong.controller.exception;
+
+import jakarta.validation.ConstraintViolationException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+@Slf4j
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler
+    public ErrorResponse handleBindingException(BindException e) {
+        log.warn(e.getMessage());
+
+        return new ErrorResponse(e.getBindingResult());
+    }
+
+    @ExceptionHandler
+    public ErrorResponse handleConstraintViolationException(ConstraintViolationException e) {
+        log.warn(e.getMessage());
+
+        return new ErrorResponse(e.getConstraintViolations());
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ErrorResponse handleException(Exception e) {
+        log.error(e.getMessage());
+
+        return new ErrorResponse("서버 오류가 발생했습니다. 관리자에게 문의하세요.");
+    }
+}
