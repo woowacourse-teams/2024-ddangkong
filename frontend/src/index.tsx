c0994071a6e9ee8c3f2ae3c08fc53e3279a17bee
rbgksqkr <rbgks1937@naver.com>
2024-07-16 13:02:27 +0900

    feat: useQuestionQuery을 구현하여 API 호출 #11
    Co-authored-by: Yuseon Kim(썬데이) <useon@users.noreply.github.com>
    Co-authored-by: novice0840 <novice0840@users.noreply.github.com>

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,9 +1,12 @@
 import { Global, ThemeProvider } from '@emotion/react';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import ReactDOM from 'react-dom/client';
 
 import App from './App';
 import GlobalStyle from './styles/GlobalStyle';
 import { Theme } from './styles/Theme';
+
+const queryClient = new QueryClient();
 
 const enableMocking = async () => {
   if (process.env.NODE_ENV !== 'development') {
@@ -17,9 +20,11 @@ const enableMocking = async () => {
 
 enableMocking().then(() => {
   ReactDOM.createRoot(document.getElementById('root')!).render(
-    <ThemeProvider theme={Theme}>
-      <Global styles={GlobalStyle} />
-      <App />
-    </ThemeProvider>,
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={Theme}>
+        <Global styles={GlobalStyle} />
+        <App />
+      </ThemeProvider>
+    </QueryClientProvider>,
   );
 });
