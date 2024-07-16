cdc79c900d64b24a1598b46b7c71825a2d791423
rbgksqkr <rbgks1937@naver.com>
2024-07-16 10:20:31 +0900

    feat: 페이지 구현 및 라우터 연결 #11
    Co-authored-by: novice0840 <novice0840@users.noreply.github.com>
    Co-authored-by: Yuseon Kim(썬데이) <useon@users.noreply.github.com>

--- frontend/src/pages/GamePage/GamePage.tsx
+++ frontend/src/pages/GamePage/GamePage.tsx
@@ -1,5 +1,5 @@
-import Button from '@/components/common/Button/Button';
 import Header from '@/components/layout/Header/Header';
+import SelectContainer from '@/components/SelectContainer/SelectContainer';
 import TopicContainer from '@/components/TopicContainer/TopicContainer';
 
 const GamePage = () => {
@@ -7,7 +7,7 @@ const GamePage = () => {
     <>
       <Header title="밸런스 게임" />
       <TopicContainer category="연애" title="우정 vs 사랑" />
-      <Button text="선택" active={true} onClick={() => console.log('클릭')} />
+      <SelectContainer />
     </>
   );
 };
---
+++ frontend/src/pages/GameResultPage/GameResultPage.tsx
@@ -0,0 +1,13 @@
+import GameResult from '@/components/GameResult/GameResult';
+import Header from '@/components/layout/Header/Header';
+
+const GameResultPage = () => {
+  return (
+    <div>
+      <Header title="밸런스 게임" />
+      <GameResult />
+    </div>
+  );
+};
+
+export default GameResultPage;
