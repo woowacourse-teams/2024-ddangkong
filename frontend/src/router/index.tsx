280d2ef812ee711167bac2e239e7e7930c042b3a
rbgksqkr <rbgks1937@naver.com>
2024-07-16 13:06:02 +0900

    feat: 공통 Header 컴포넌트 공용화 #11
    Co-authored-by: Yuseon Kim(썬데이) <useon@users.noreply.github.com>
    Co-authored-by: novice0840 <novice0840@users.noreply.github.com>

--- frontend/src/router/index.tsx
+++ frontend/src/router/index.tsx
@@ -1,5 +1,7 @@
 import { createBrowserRouter } from 'react-router-dom';
 
+import { Layout } from './layout';
+
 import GamePage from '@/pages/GamePage/GamePage';
 import GameResultPage from '@/pages/GameResultPage/GameResultPage';
 import RoundResultPage from '@/pages/RoundResultPage/RoundResultPage';
@@ -7,6 +9,7 @@ import RoundResultPage from '@/pages/RoundResultPage/RoundResultPage';
 export const router = createBrowserRouter([
   {
     path: '/',
+    element: <Layout />,
     children: [
       {
         index: true,
