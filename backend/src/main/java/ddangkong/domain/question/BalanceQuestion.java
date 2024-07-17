71db0ecc546413c6af6ae2c52d79886b30521daf
DESKTOP-E5VRH4Q\cnddk <cnddkscndgus@naver.com>
2024-07-17 16:51:33 +0900

    feat: ERD에 따른 Entity 구현

---
+++ backend/src/main/java/ddangkong/domain/question/BalanceQuestion.java
@@ -0,0 +1,33 @@
+package ddangkong.domain.question;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@EqualsAndHashCode
+@ToString
+public class BalanceQuestion {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Category category;
+
+    @Column(nullable = false)
+    private String content;
+}
