5ff5dfef1de097d655ea89705ac9e6bd8de30b90
rbgksqkr <rbgks1937@naver.com>
2024-07-20 01:25:28 +0900

    refactor: webpack source map 설정 수정 #25

--- frontend/webpack.config.dev.js
+++ frontend/webpack.config.dev.js
@@ -4,7 +4,7 @@ const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
 
 module.exports = merge(common, {
   mode: 'development',
-  devtool: 'inline-source-map',
+  devtool: 'eval-source-map',
   devServer: {
     port: 3000,
     historyApiFallback: true,
