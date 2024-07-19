74329c69b23f9dab16e40ddb6b3bdd2ec99fbbea
rbgksqkr <rbgks1937@naver.com>
2024-07-19 21:35:14 +0900

    design: peanut500 색 및 동적 스타일링 수정 #25

--- frontend/src/components/common/Button/Button.styled.ts
+++ frontend/src/components/common/Button/Button.styled.ts
@@ -1,10 +1,11 @@
 import { css } from '@emotion/react';
+
 import { Theme } from '@/styles/Theme';
 
 export const buttonLayout = (active: boolean) => css`
   padding: 1rem 4rem;
 
-  background-color: ${active ? Theme.color.peanut500 : Theme.color.peanut300};
+  background-color: ${active ? Theme.color.peanut400 : Theme.color.peanut300};
 
   font-weight: bold;
   font-size: 2rem;
