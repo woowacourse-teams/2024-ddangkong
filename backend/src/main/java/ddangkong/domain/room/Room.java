c039eb6f2b93822e5ed179a8abeee00d4faefd1f
leegwichan <cnddkscndgus@naver.com>
2024-07-20 15:10:27 +0900

    refactor: 지연 로딩의 여파를 막기 위해, 엔티티의 @EqualsAndHashCode, @ToString 제거 #28

--- backend/src/main/java/ddangkong/domain/room/Room.java
+++ backend/src/main/java/ddangkong/domain/room/Room.java
@@ -5,16 +5,12 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.AccessLevel;
-import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.ToString;
 
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
-@EqualsAndHashCode
-@ToString
 public class Room {
 
     @Id
