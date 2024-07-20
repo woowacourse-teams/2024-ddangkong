935ff1edd61c5b469d9d5ce6458560e41d508398
leegwichan <cnddkscndgus@naver.com>
2024-07-20 15:07:21 +0900

    refactor: Repository 를 domain 패키지로 이동 및 필요없는 에노테이션 제거 #28

--- backend/src/main/java/ddangkong/domain/option/BalanceOptionRepository.java
+++ backend/src/main/java/ddangkong/domain/option/BalanceOptionRepository.java
@@ -1,12 +1,9 @@
-package ddangkong.repository.option;
+package ddangkong.domain.option;
 
-import ddangkong.domain.option.BalanceOption;
 import ddangkong.domain.question.BalanceQuestion;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface BalanceOptionRepository extends JpaRepository<BalanceOption, Long> {
 
     List<BalanceOption> findByBalanceQuestion(BalanceQuestion balanceQuestion);
--- backend/src/test/java/ddangkong/domain/BaseRepositoryTest.java
+++ backend/src/test/java/ddangkong/domain/BaseRepositoryTest.java
@@ -1,4 +1,4 @@
-package ddangkong.repository;
+package ddangkong.domain;
 
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.jdbc.Sql;
