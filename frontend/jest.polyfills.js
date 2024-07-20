035bd51b68531db8940e158ef428a15c01da6f1e
rbgksqkr <rbgks1937@naver.com>
2024-07-21 00:15:10 +0900

    fix: 라이브러리 버전에 영향받지 않도록 jest polyfill 수정 #25

--- frontend/jest.polyfills.js
+++ frontend/jest.polyfills.js
@@ -10,15 +10,18 @@
  */
 
 const { TextDecoder, TextEncoder } = require('node:util');
+const { ReadableStream, TransformStream } = require('node:stream/web');
 
 Object.defineProperties(globalThis, {
   TextDecoder: { value: TextDecoder },
   TextEncoder: { value: TextEncoder },
+  ReadableStream: { value: ReadableStream },
+  TransformStream: { value: TransformStream },
 });
 
 const { Blob, File } = require('node:buffer');
 
-const { fetch, Headers, FormData, Request, Response, ReadableStream } = require('undici');
+const { fetch, Headers, FormData, Request, Response } = require('undici');
 
 Object.defineProperties(globalThis, {
   fetch: { value: fetch, writable: true },
@@ -28,5 +31,4 @@ Object.defineProperties(globalThis, {
   FormData: { value: FormData },
   Request: { value: Request },
   Response: { value: Response },
-  ReadableStream: { value: ReadableStream },
 });
