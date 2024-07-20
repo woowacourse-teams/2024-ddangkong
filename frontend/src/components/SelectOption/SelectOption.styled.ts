3d3bf0c246ceb253b618147222923aee64567cdc
rbgksqkr <rbgks1937@naver.com>
2024-07-21 00:14:22 +0900

    style: 불필요한 주석 제거 #25

--- frontend/src/components/SelectOption/SelectOption.styled.ts
+++ frontend/src/components/SelectOption/SelectOption.styled.ts
@@ -12,14 +12,14 @@ export const layout = (selected: boolean) => css`
 
   background-color: ${selected ? Theme.color.peanut500 : Theme.color.peanut300};
 
-  color: #000; /* 텍스트 색상 */
+  color: #000;
   font-weight: bold;
   font-size: 1.6rem;
   line-height: 2.4rem;
   text-align: center;
 
   word-break: keep-all;
-  border-radius: 3rem; /* 둥근 모서리 */
+  border-radius: 3rem;
 
   transition: all 0.5s;
   scale: ${selected ? 1.1 : 1};
