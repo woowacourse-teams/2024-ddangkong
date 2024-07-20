01cedd8cd257780336d9a3cd24447d9530511a82
rbgksqkr <rbgks1937@naver.com>
2024-07-21 00:18:25 +0900

    test: TopicContainer 기본 스토리 추가 #25

---
+++ frontend/src/components/TopicContainer/TopicContainer.stories.tsx
@@ -0,0 +1,16 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import TopicContainer from './TopicContainer';
+
+const meta = {
+  title: 'TopicContainer',
+  component: TopicContainer,
+} satisfies Meta<typeof TopicContainer>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const 기본_카테고리_및_질문: Story = {
+  render: (args) => <TopicContainer {...args} />,
+};
