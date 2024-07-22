7585e9d91f1a41298903d7dceaabdbe1b35f200e
Nam Gi Beom <rlqja0523@naver.com>
2024-07-23 01:29:25 +0900

    test: extension 적용 #37

--- backend/src/test/java/ddangkong/controller/BaseControllerTest.java
+++ backend/src/test/java/ddangkong/controller/BaseControllerTest.java
@@ -1,13 +1,18 @@
 package ddangkong.controller;
 
 
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+
+import ddangkong.support.extension.DatabaseCleanerExtension;
 import io.restassured.RestAssured;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.test.context.jdbc.Sql;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ExtendWith(DatabaseCleanerExtension.class)
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 @Sql(scripts = "/init-test.sql")
 public abstract class BaseControllerTest {
 
