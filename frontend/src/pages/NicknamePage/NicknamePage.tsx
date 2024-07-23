561c10ab9f4eb1fed1a01ee9b81dc8cb84a91e3e
novice0840 <magnolia0840@gmail.com>
2024-07-23 19:01:35 +0900

    feat: nickname page UI #46

--- frontend/src/pages/NicknamePage/NicknamePage.tsx
+++ frontend/src/pages/NicknamePage/NicknamePage.tsx
@@ -1,7 +1,17 @@
-import React from 'react';
+import { profile, nickname, nicknameInputWrapper, nicknameInput } from './NicknamePage.styled';
+
+import Content from '@/components/layout/Content/Content';
 
 const NicknamePage = () => {
-  return <div>this is Nick name page</div>;
+  return (
+    <Content>
+      <div css={profile}></div>
+      <div css={nickname}>닉네임</div>
+      <div css={nicknameInputWrapper}>
+        <input css={nicknameInput} type="text" placeholder="닉네임을 입력해 주세요" />
+      </div>
+    </Content>
+  );
 };
 
 export default NicknamePage;
