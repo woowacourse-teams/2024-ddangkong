93dd3c33405a7fa6eb7d2ee8c317bbeae830c8fb
rbgksqkr <rbgks1937@naver.com>
2024-07-25 02:25:04 +0900

    refactor: placeholderData를 이용하여 초기값을 설정하고, 라운드 결과 애니메이션 적용 #50

--- frontend/src/hooks/useRoundVoteResultQuery.ts
+++ frontend/src/hooks/useRoundVoteResultQuery.ts
@@ -1,14 +1,15 @@
 import { useQuery } from '@tanstack/react-query';
 
+import INITIAL_VALUE from '../mocks/data/roundVoteResultInitialValue.json';
+
 import { fetchRoundVoteResult } from '@/apis/balanceContent';
 
 const useRoundVoteResultQuery = () => {
   const roundVoteResultQuery = useQuery({
     queryKey: ['roundVoteResult'],
     queryFn: async () => await fetchRoundVoteResult(),
+    placeholderData: INITIAL_VALUE,
   });
-
-  roundVoteResultQuery.data?.group;
 
   return {
     ...roundVoteResultQuery,
