8900a2946aef602d6a935dfca34a0965b74a8d43
DESKTOP-E5VRH4Q\cnddk <cnddkscndgus@naver.com>
2024-07-24 13:02:33 +0900

    style: 중복을 줄이기 위해, exception 이름 변경 #52
    InternalServerErrorException 에서 InternalServerException 으로 변경

---
+++ backend/src/main/java/ddangkong/exception/InternalServerException.java
@@ -0,0 +1,8 @@
+package ddangkong.exception;
+
+public class InternalServerException extends RuntimeException {
+
+    public InternalServerException(String message) {
+        super(message);
+    }
+}
