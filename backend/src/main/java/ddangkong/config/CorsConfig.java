1385e74827d66892cec435f7981a4ba99ae42246
Nam Gi Beom <rlqja0523@naver.com>
2024-07-25 11:17:56 +0900

    feat: CORS 설정 추가 #57

---
+++ backend/src/main/java/ddangkong/config/CorsConfig.java
@@ -0,0 +1,27 @@
+package ddangkong.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    private final String corsOrigin;
+
+    public CorsConfig(@Value("${cors.origin}") String corsOrigin) {
+        this.corsOrigin = corsOrigin;
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins(corsOrigin)
+                .allowedMethods(
+                        HttpMethod.GET.name(), HttpMethod.POST.name(), HttpMethod.PATCH.name(), HttpMethod.DELETE.name()
+                )
+                .allowedHeaders("*");
+    }
+}
