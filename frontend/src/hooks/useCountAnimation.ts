8defa35fcb6e810434a88b0f435b5f36287d8221
rbgksqkr <rbgks1937@naver.com>
2024-07-25 20:50:29 +0900

    style: 카운팅 애니메이션 주석 추가 #50

--- frontend/src/hooks/useCountAnimation.ts
+++ frontend/src/hooks/useCountAnimation.ts
@@ -11,13 +11,15 @@ interface UseCountAnimationProps {
   duration?: number;
 }
 
+// start에서 target으로 숫자 카운팅되는 애니메이션 커스텀 훅
 const useCountAnimation = ({ target, start = 50, duration = 2000 }: UseCountAnimationProps) => {
   const [count, setCount] = useState(start);
   const frameRate = 500 / 60;
   const totalFrame = Math.round(duration / frameRate);
 
   useEffect(() => {
-    if (typeof target === 'undefined' || target === start) return; // target 값을 API로 불러올 경우 초기값이 애니메이션에 반영되므로 예외처리
+    // target 값을 API로 불러올 경우 초기값이 애니메이션에 반영되므로 예외처리
+    if (typeof target === 'undefined' || target === start) return;
     let currentNumber = start;
     const counter = setInterval(() => {
       const progress = easeOutRate(++currentNumber / totalFrame);
