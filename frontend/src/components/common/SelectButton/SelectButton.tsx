5249d5504905043ed75f36f1b01714907aa3c32b
rbgksqkr <rbgks1937@naver.com>
2024-07-27 11:24:53 +0900

    feat: SelectButton에 투표하는 API 호출 및 구현 #82

--- frontend/src/components/common/SelectButton/SelectButton.tsx
+++ frontend/src/components/common/SelectButton/SelectButton.tsx
@@ -3,14 +3,27 @@ import { useNavigate } from 'react-router-dom';
 import Button from '../Button/Button';
 import { bottomButtonLayout } from '../Button/Button.styled';
 
+import { voteBalanceContent } from '@/apis/balanceContent';
+import useBalanceContentQuery from '@/hooks/useBalanceContentQuery';
+
 interface SelectButtonProps {
   isDisabled: boolean;
+  selectedId: number;
 }
 
-const SelectButton = ({ isDisabled }: SelectButtonProps) => {
+const SelectButton = ({ isDisabled, selectedId }: SelectButtonProps) => {
   const navigate = useNavigate();
+  const { balanceContent } = useBalanceContentQuery();
 
-  const goToRoundResult = () => {
+  const goToRoundResult = async () => {
+    if (!balanceContent) return;
+
+    await voteBalanceContent({
+      optionId: selectedId,
+      contentId: balanceContent.contentId,
+      roomId: 1,
+    });
+
     navigate(`/round/result`);
   };
 
