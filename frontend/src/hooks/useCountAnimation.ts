f7db8090d30e9adc8295c670af3f2c4d18aaecc6
rbgksqkr <rbgks1937@naver.com>
2024-07-28 12:04:07 +0900

    fix: 불필요한 effect 의존성 제거 #82

--- frontend/src/hooks/useCountAnimation.ts
+++ frontend/src/hooks/useCountAnimation.ts
@@ -1,6 +1,8 @@
 import { useEffect, useState } from 'react';
 
-// "ease out" : 빨라졌다가 천천히 끝나는 애니메이션
+const FRAME_RATE = 500 / 60;
+
+// 빨라졌다가 천천히 끝나는 애니메이션
 const easeOutRate = (timingRate: number) => {
   return timingRate === 1 ? 1 : 1 - Math.pow(2, -10 * timingRate);
 };
@@ -14,13 +16,14 @@ interface UseCountAnimationProps {
 // start에서 target으로 숫자 카운팅되는 애니메이션 커스텀 훅
 const useCountAnimation = ({ target, start = 50, duration = 2000 }: UseCountAnimationProps) => {
   const [count, setCount] = useState(start);
-  const frameRate = 500 / 60;
-  const totalFrame = Math.round(duration / frameRate);
 
   useEffect(() => {
     // target 값을 API로 불러올 경우 초기값이 애니메이션에 반영되므로 예외처리
     if (typeof target === 'undefined' || target === start) return;
+
+    const totalFrame = Math.round(duration / FRAME_RATE);
     let currentNumber = start;
+
     const counter = setInterval(() => {
       const progress = easeOutRate(++currentNumber / totalFrame);
       setCount(Math.round(target * progress));
@@ -28,8 +31,8 @@ const useCountAnimation = ({ target, start = 50, duration = 2000 }: UseCountAnim
       if (progress === 1) {
         clearInterval(counter);
       }
-    }, frameRate);
-  }, [target, frameRate, start, totalFrame]);
+    }, FRAME_RATE);
+  }, [target, start, duration]);
 
   return count;
 };
