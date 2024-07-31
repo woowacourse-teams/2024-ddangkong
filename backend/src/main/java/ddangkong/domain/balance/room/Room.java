2b745a82aad906d893cfeeb3337d6cf99a6a65fe
Nam Gi Beom <rlqja0523@naver.com>
2024-07-31 17:41:15 +0900

    feat: 방의 라운드 종료 여부 확인 기능 추가 #93

--- backend/src/main/java/ddangkong/domain/balance/room/Room.java
+++ backend/src/main/java/ddangkong/domain/balance/room/Room.java
@@ -7,9 +7,11 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Getter
+@NoArgsConstructor // TODO protected로 변경
 public class Room {
 
     private static final int DEFAULT_TOTAL_ROUND = 5;
@@ -25,6 +27,11 @@ public class Room {
     @Column(nullable = false)
     private int currentRound = START_ROUND;
 
+    public Room(int totalRound, int currentRound) {
+        this.totalRound = totalRound;
+        this.currentRound = currentRound;
+    }
+
     public void moveToNextRound() {
         if (canMoveToNextRound()) {
             currentRound++;
@@ -35,5 +42,24 @@ public class Room {
 
     private boolean canMoveToNextRound() {
         return currentRound < totalRound;
+    }
+
+    public boolean isMyRoundFinished(int myRound) {
+        if (myRound < START_ROUND) {
+            throw new BadRequestException("나의 라운드는 %d보다 크거나 같아야 합니다. myRound : %d"
+                    .formatted(START_ROUND, myRound)
+            );
+        }
+        if (currentRound < myRound) {
+            throw new BadRequestException("방의 currentRound보다 작거나 같아야 합니다. currentRound : %d, myRound : %d"
+                    .formatted(currentRound, myRound)
+            );
+        }
+        return currentRound != myRound;
+    }
+
+    public boolean isAllRoundFinished() {
+        // TODO: room의 status가 종료인지 여부 리턴
+        return currentRound == totalRound;
     }
 }
--- backend/src/test/java/ddangkong/domain/balance/room/RoomTest.java
+++ backend/src/test/java/ddangkong/domain/balance/room/RoomTest.java
@@ -47,4 +47,83 @@ class RoomTest {
             }
         }
     }
+
+    @Nested
+    class 라운드_종료 {
+
+        private static final int FIXED_TOTAL_ROUND = 5;
+
+        @Test
+        void 나의_라운드가_종료되었으면_true를_반환한다() {
+            // given
+            int currentRound = 2;
+            Room room = new Room(FIXED_TOTAL_ROUND, currentRound);
+            int myRound = 1;
+
+            // when
+            boolean actual = room.isMyRoundFinished(myRound);
+
+            // then
+            assertThat(actual).isTrue();
+        }
+
+        @Test
+        void 나의_라운드가_종료되지_않았으면_false를_반환한다() {
+            // given
+            int currentRound = 2;
+            Room room = new Room(FIXED_TOTAL_ROUND, currentRound);
+            int myRound = 2;
+
+            // when
+            boolean actual = room.isMyRoundFinished(myRound);
+
+            // then
+            assertThat(actual).isFalse();
+        }
+
+        @Test
+        void 나의_라운드가_방의_시작_라운드보다_작으면_예외가_발생한다() {
+            // given
+            Room room = new Room(FIXED_TOTAL_ROUND, 1);
+            int invalidMyRound = 0;
+
+            // when & then
+            assertThatThrownBy(() -> room.isMyRoundFinished(invalidMyRound))
+                    .isExactlyInstanceOf(BadRequestException.class)
+                    .hasMessageContaining("나의 라운드는 1보다 크거나 같아야 합니다. myRound : 0");
+        }
+
+        @Test
+        void 나의_라운드가_방의_현재_라운드보다_크면_예외가_발생한다() {
+            // given
+            int currentRound = 1;
+            Room room = new Room(FIXED_TOTAL_ROUND, currentRound);
+            int invalidMyRound = 2;
+
+            // when & then
+            assertThatThrownBy(() -> room.isMyRoundFinished(invalidMyRound))
+                    .isExactlyInstanceOf(BadRequestException.class)
+                    .hasMessageContaining("방의 currentRound보다 작거나 같아야 합니다. currentRound : 1, myRound : 2");
+        }
+
+        @Test
+        void 전체_라운드가_종료되었으면_true를_반환한다() { // TODO
+            // given
+
+            // when
+
+            // then
+
+        }
+
+        @Test
+        void 전체_라운드가_종료되지_않았으면_false를_반환한다() { // TODO
+            // given
+
+            // when
+
+            // then
+
+        }
+    }
 }
