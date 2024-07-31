4e53d2ad7452c414c2bf02df3a25b118512b60d3
novice0840 <magnolia0840@gmail.com>
2024-07-31 16:54:54 +0900

    fix: css 순서 조정 #92

--- frontend/src/components/ReadyMembersContainer/ReadyMembersContainer.styled.ts
+++ frontend/src/components/ReadyMembersContainer/ReadyMembersContainer.styled.ts
@@ -3,18 +3,16 @@ import { css } from '@emotion/react';
 import { Theme } from '@/styles/Theme';
 
 export const readyMembersContainerLayout = css`
+  position: relative;
   height: 25rem;
   padding: 2rem 3rem 0;
+  border-radius: 2rem;
 
   background-color: ${Theme.color.peanut300};
 
-  position: relative;
-
-  border-radius: 2rem;
+  font-weight: 600;
 
   overflow-y: scroll;
-
-  font-weight: 600;
 
   /* 스크롤바 숨기기 */
   scrollbar-width: none; /* Firefox */
@@ -26,9 +24,10 @@ export const readyMembersContainerLayout = css`
 `;
 
 export const totalNumber = css`
+  padding-left: 2rem;
+
   font-weight: 900;
   font-size: 1.2rem;
-  padding-left: 2rem;
 `;
 
 export const memberList = css`
@@ -39,20 +38,19 @@ export const memberList = css`
 
 export const memberItem = css`
   display: flex;
-  gap: 2rem;
   align-items: center;
+  gap: 2rem;
 `;
 
 export const profileBox = css`
-  width: 3.6rem;
-  height: 3.6rem;
-  background-color: white;
-
-  border-radius: 50%;
-
   display: flex;
   justify-content: center;
   align-items: center;
+  width: 3.6rem;
+  height: 3.6rem;
+  border-radius: 50%;
+
+  background-color: white;
 `;
 
 export const memberStatus = css`
