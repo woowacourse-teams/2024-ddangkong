086cd19d4b9432ec1520649b46b3a0449899f883
novice0840 <magnolia0840@gmail.com>
2024-08-01 17:55:29 +0900

    feat: categoryContainer 스토리 북 추가 #92

--- frontend/src/components/CategoryContainer/CategoryContainer.tsx
+++ frontend/src/components/CategoryContainer/CategoryContainer.tsx
@@ -2,11 +2,15 @@ import React from 'react';
 
 import { categoryContainerLayout, title, subtitle } from './CategoryContainer.styled';
 
-const CategoryContainer = () => {
+interface CategoryContainerInterface {
+  category: string;
+}
+
+const CategoryContainer = ({ category }: CategoryContainerInterface) => {
   return (
     <section css={categoryContainerLayout}>
       <h2 css={subtitle}>카테고리</h2>
-      <h1 css={title}>연애</h1>
+      <h1 css={title}>{category}</h1>
     </section>
   );
 };
