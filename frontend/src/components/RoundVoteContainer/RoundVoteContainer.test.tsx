fbfc732d217031b02ac9abc107e71d7e2788eaf6
rbgksqkr <rbgks1937@naver.com>
2024-08-01 15:26:46 +0900

    test: 전체 탭을 클릭하면 전체 사용자 통계를 퍼센트로 보여주는 테스트 코드 작성 #97

--- frontend/src/components/RoundVoteContainer/RoundVoteContainer.test.tsx
+++ frontend/src/components/RoundVoteContainer/RoundVoteContainer.test.tsx
@@ -1,4 +1,5 @@
 import { screen, waitFor } from '@testing-library/react';
+import { userEvent } from '@testing-library/user-event';
 import { customRender } from 'test-utils';
 
 import RoundVoteContainer from './RoundVoteContainer';
@@ -11,6 +12,22 @@ describe('RoundVoteContainer 컴포넌트 테스트', () => {
       () => {
         expect(screen.getByText('73%')).toBeInTheDocument();
         expect(screen.getByText('27%')).toBeInTheDocument();
+      },
+      { timeout: 3000 },
+    );
+  });
+  it('기본 탭인 그룹 탭에서, 전체 탭을 클릭하면 전체 사용자 통계를 퍼센트로 보여준다.', async () => {
+    const user = userEvent.setup();
+    customRender(<RoundVoteContainer />);
+
+    const button = await screen.findByRole('button', { name: '전체' });
+
+    await user.click(button);
+
+    await waitFor(
+      () => {
+        expect(screen.getByText('16%')).toBeInTheDocument();
+        expect(screen.getByText('84%')).toBeInTheDocument();
       },
       { timeout: 3000 },
     );
