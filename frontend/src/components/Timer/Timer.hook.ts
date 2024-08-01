9f6bb0e1140bcb91fd1e83701660831a6802b973
rbgksqkr <rbgks1937@naver.com>
2024-08-02 00:38:22 +0900

    feat: 서버에서 timeLimit을 받아 타이머 설정 #97

--- frontend/src/components/Timer/Timer.hook.ts
+++ frontend/src/components/Timer/Timer.hook.ts
@@ -1,10 +1,15 @@
 import { useEffect, useRef, useState } from 'react';
 
-const INITIAL_TIMER = 30; // TODO: tanstack-query로 timeLimit 서버 데이터 받아오기
+import useBalanceContentQuery from '@/hooks/useBalanceContentQuery';
+
 const INITIAL_WIDTH = 100;
+const DELAY = 1000;
 
 const useRoundTimer = () => {
-  const [timerCount, setTimerCount] = useState(INITIAL_TIMER);
+  const { balanceContent } = useBalanceContentQuery();
+  const timeLimit = balanceContent?.timeLimit || 30;
+
+  const [timerCount, setTimerCount] = useState(timeLimit);
   const [barWidth, setBarWidth] = useState(INITIAL_WIDTH);
   const isAlmostFinished = timerCount <= 5;
 
@@ -17,15 +22,20 @@ const useRoundTimer = () => {
   }, [timerCount]);
 
   useEffect(() => {
+    if (!balanceContent) return;
+
+    const DECREASE_RATE = INITIAL_WIDTH / timeLimit;
+    setTimerCount(timeLimit);
+
     timeout.current = setInterval(() => {
       setTimerCount((prev) => prev - 1);
-      setBarWidth((prevWidth) => (prevWidth > 0 ? prevWidth - INITIAL_WIDTH / INITIAL_TIMER : 0));
-    }, 1000);
+      setBarWidth((prevWidth) => (prevWidth > 0 ? prevWidth - DECREASE_RATE : 0));
+    }, DELAY);
 
     return () => {
       clearInterval(timeout.current);
     };
-  }, []);
+  }, [balanceContent, timeLimit]);
 
   return { timerCount, barWidth, isAlmostFinished };
 };
