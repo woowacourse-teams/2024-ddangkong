5c42509fe2615a64ba4960324b40ddd42d946d20
DESKTOP-E5VRH4Q\cnddk <cnddkscndgus@naver.com>
2024-08-02 15:53:26 +0900

    feat: 랜덤으로 컨텐츠를 N개 조회하는 기능 구현 #99

--- backend/src/main/java/ddangkong/domain/balance/content/BalanceContentRepository.java
+++ backend/src/main/java/ddangkong/domain/balance/content/BalanceContentRepository.java
@@ -1,13 +1,33 @@
 package ddangkong.domain.balance.content;
 
-import ddangkong.domain.balance.option.BalanceOption;
+import static java.util.stream.Collectors.toList;
+
 import ddangkong.exception.BadRequestException;
+import ddangkong.exception.InternalServerException;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.LongStream;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BalanceContentRepository extends JpaRepository<BalanceContent, Long> {
 
+    long count();
+
+    List<BalanceContent> findByIdIn(List<Long> ids);
+
     default BalanceContent getById(Long id) {
         return findById(id)
                 .orElseThrow(() -> new BadRequestException("해당 질문 컨텐츠가 존재하지 않습니다."));
+    }
+
+    default List<BalanceContent> findByRandom(int count) {
+        List<Long> ids = LongStream.rangeClosed(1, count).boxed().collect(toList());
+        if (ids.size() < count) {
+            throw new InternalServerException("질문이 라운드만큼 존재하지 않습니다.");
+        }
+
+        Collections.shuffle(ids);
+        List<Long> candidateIds = ids.subList(0, count);
+        return findByIdIn(candidateIds);
     }
 }
