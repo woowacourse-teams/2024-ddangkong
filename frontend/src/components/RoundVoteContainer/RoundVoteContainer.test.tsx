e5d7ee82b2b80b214a2891dd00b26119e7d32111
rbgksqkr <rbgks1937@naver.com>
2024-08-03 21:07:20 +0900

    test: 컴포넌트 역할 분리로 인한 테스트 코드 수정 #97

--- frontend/src/components/RoundVoteContainer/RoundVoteContainer.test.tsx
+++ frontend/src/components/RoundVoteContainer/RoundVoteContainer.test.tsx
@@ -5,17 +5,6 @@ import { customRender } from 'test-utils';
 import RoundVoteContainer from './RoundVoteContainer';
 
 describe('RoundVoteContainer 컴포넌트 테스트', () => {
-  it('라운드 결과 그룹원들이 선택한 퍼센트를 카운팅 애니메이션으로 보여준다.', async () => {
-    customRender(<RoundVoteContainer />);
-
-    await waitFor(
-      () => {
-        expect(screen.getByText('73%')).toBeInTheDocument();
-        expect(screen.getByText('27%')).toBeInTheDocument();
-      },
-      { timeout: 3000 },
-    );
-  });
   it('기본 탭인 그룹 탭에서, 전체 탭을 클릭하면 전체 사용자 통계를 퍼센트로 보여준다.', async () => {
     const user = userEvent.setup();
     customRender(<RoundVoteContainer />);
