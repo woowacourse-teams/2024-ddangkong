654540dc1bdc2dc89cb4f1dd71d5a7ad01923e7a
Nam Gi Beom <rlqja0523@naver.com>
2024-08-03 20:13:56 +0900

    feat: RoomContent 컨텐츠 사용 여부, 라운드 일치 여부 검증 기능 추가 #58

--- backend/src/main/java/ddangkong/domain/balance/room/RoomContent.java
+++ backend/src/main/java/ddangkong/domain/balance/room/RoomContent.java
@@ -43,6 +43,18 @@ public class RoomContent extends BaseEntity {
     @Column(nullable = false)
     private boolean isUsed;
 
+    public RoomContent(Room room,
+                       BalanceContent balanceContent,
+                       int round,
+                       LocalDateTime roundEndedAt,
+                       boolean isUsed) {
+        this.room = room;
+        this.balanceContent = balanceContent;
+        this.round = round;
+        this.roundEndedAt = roundEndedAt;
+        this.isUsed = isUsed;
+    }
+
     public boolean isRoundOver(LocalDateTime currentTime) {
         return currentTime.isAfter(getRoundEndedAt());
     }
@@ -72,5 +84,18 @@ public class RoomContent extends BaseEntity {
             throw new BadRequestException("라운드 종료 시간이 설정되지 않습니다.");
         }
         return roundEndedAt;
+    }
+
+    public void validateAlreadyUsed() {
+        if (isUsed) {
+            throw new BadRequestException("이미 사용된 컨텐츠입니다.");
+        }
+    }
+
+    public void validateSameRound(int round) {
+        if (this.round != round) {
+            throw new BadRequestException("컨텐츠의 라운드가 일치하지 않습니다. 방 컨텐츠의 라운드 : %d, 요청한 라운드 : %d"
+                    .formatted(this.round, round));
+        }
     }
 }
