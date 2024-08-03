baec679b655aef4e6f7a1dca86f61316a8c0d3e5
Nam Gi Beom <rlqja0523@naver.com>
2024-08-03 18:10:40 +0900

    feat: fixed clock extension 추가 #58

--- backend/src/test/java/ddangkong/controller/BaseControllerTest.java
+++ backend/src/test/java/ddangkong/controller/BaseControllerTest.java
@@ -5,14 +5,17 @@ import static org.springframework.boot.test.context.SpringBootTest.WebEnvironmen
 
 import ddangkong.support.extension.DatabaseCleanerExtension;
 import io.restassured.RestAssured;
+import java.time.Clock;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.SpyBean;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.test.context.jdbc.Sql;
 
 @ExtendWith(DatabaseCleanerExtension.class)
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+@SpyBean(Clock.class)
 @Sql(scripts = "/init-test.sql")
 public abstract class BaseControllerTest {
 
--- backend/src/test/java/ddangkong/service/BaseServiceTest.java
+++ backend/src/test/java/ddangkong/service/BaseServiceTest.java
@@ -3,12 +3,15 @@ package ddangkong.service;
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 
 import ddangkong.support.extension.DatabaseCleanerExtension;
+import java.time.Clock;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.SpyBean;
 import org.springframework.test.context.jdbc.Sql;
 
 @ExtendWith(DatabaseCleanerExtension.class)
 @SpringBootTest(webEnvironment = WebEnvironment.NONE)
+@SpyBean(Clock.class)
 @Sql(scripts = "/init-test.sql")
 public abstract class BaseServiceTest {
 }
