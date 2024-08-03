5b8313c1758ea516e0a7244f5c1c0b4a098b96f8
useon <roqkftjs@gmail.com>
2024-08-03 21:27:59 +0900

    feat: 투표 현황 페이지에서 게임 진행 상태에 따라 페이지 라우팅 구현 #106

--- frontend/src/components/OptionParticipantsContainer/OptionParticipantsContainer.hook.ts
+++ frontend/src/components/OptionParticipantsContainer/OptionParticipantsContainer.hook.ts
@@ -1,5 +1,7 @@
-import { useLocation } from 'react-router-dom';
+import { useEffect } from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
 
+import { ROUTES } from '@/constants/routes';
 import useBalanceContentQuery from '@/hooks/useBalanceContentQuery';
 import useMyGameStatusQuery from '@/hooks/useMyGameStatusQuery';
 import useRoundVoteResultQuery from '@/hooks/useRoundVoteResultQuery';
@@ -7,6 +9,7 @@ import useRoundVoteResultQuery from '@/hooks/useRoundVoteResultQuery';
 const useOptionParticipants = () => {
   const { search } = useLocation();
   const roomId = Number(new URLSearchParams(search).get('roomId'));
+  const navigate = useNavigate();
 
   const { balanceContent } = useBalanceContentQuery();
 
@@ -16,6 +19,15 @@ const useOptionParticipants = () => {
   });
 
   const { isRoundFinished, isGameFinished } = useMyGameStatusQuery({ balanceContent, roomId });
+
+  useEffect(() => {
+    if (isGameFinished) {
+      navigate(ROUTES.gameResult);
+    }
+    if (isRoundFinished) {
+      navigate(ROUTES.game);
+    }
+  }, [isRoundFinished, isGameFinished]);
 
   return {
     groupRoundResult,
