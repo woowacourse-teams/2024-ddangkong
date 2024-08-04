71e4401bc9cf606645f8d34a31dc9ce466e7c833
rbgksqkr <rbgks1937@naver.com>
2024-08-05 01:03:42 +0900

    fix: lint rule 무시 없이 import 하도록 수정 #97

--- frontend/src/utils/test-utils.tsx
+++ frontend/src/utils/test-utils.tsx
@@ -1,7 +1,7 @@
 import { Global, ThemeProvider } from '@emotion/react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-// eslint-disable-next-line import/named
-import { render, RenderOptions } from '@testing-library/react';
+import { render } from '@testing-library/react';
+import type { RenderOptions } from '@testing-library/react';
 import { PropsWithChildren } from 'react';
 import { MemoryRouter } from 'react-router-dom';
 import { RecoilRoot } from 'recoil';
