9a4053b83e6e187aed1d1684630fea9578f8c321
PgmJun <84304802+PgmJun@users.noreply.github.com>
2024-08-04 22:37:16 +0900

    feat: MemberFixture 다수 생성 로직 작성 #114

--- backend/src/test/java/ddangkong/support/fixture/MemberFixture.java
+++ backend/src/test/java/ddangkong/support/fixture/MemberFixture.java
@@ -3,6 +3,8 @@ package ddangkong.support.fixture;
 import ddangkong.domain.balance.room.Room;
 import ddangkong.domain.member.Member;
 import ddangkong.domain.member.MemberRepository;
+import java.util.ArrayList;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -20,5 +22,15 @@ public class MemberFixture {
     public Member createCommon(String nickname, Room room) {
         Member member = Member.createCommon(nickname, room);
         return memberRepository.save(member);
+    }
+
+    public List<Member> createCommonMembers(Room room, int count) {
+        List<Member> members = new ArrayList<>();
+        for (int i = 1; i <= count; i++) {
+            Member member = Member.createCommon("회원%d".formatted(i), room);
+            members.add(memberRepository.save(member));
+        }
+
+        return members;
     }
 }
