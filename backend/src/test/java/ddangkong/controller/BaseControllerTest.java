c7ad09f67911b410237ffac1812a83e310823bc6
PgmJun <84304802+PgmJun@users.noreply.github.com>
2024-08-04 22:17:23 +0900

    test: BaseTest 클래스에 Fixture 의존성 추가 #114

--- backend/src/test/java/ddangkong/controller/BaseControllerTest.java
+++ backend/src/test/java/ddangkong/controller/BaseControllerTest.java
@@ -4,9 +4,16 @@ package ddangkong.controller;
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 
 import ddangkong.support.extension.DatabaseCleanerExtension;
+import ddangkong.support.fixture.BalanceContentFixture;
+import ddangkong.support.fixture.BalanceOptionFixture;
+import ddangkong.support.fixture.BalanceVoteFixture;
+import ddangkong.support.fixture.MemberFixture;
+import ddangkong.support.fixture.RoomContentFixture;
+import ddangkong.support.fixture.RoomFixture;
 import io.restassured.RestAssured;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.test.context.jdbc.Sql;
@@ -23,4 +30,22 @@ public abstract class BaseControllerTest {
     void setUp() {
         RestAssured.port = port;
     }
+
+    @Autowired
+    protected BalanceContentFixture balanceContentFixture;
+
+    @Autowired
+    protected BalanceOptionFixture balanceOptionFixture;
+
+    @Autowired
+    protected BalanceVoteFixture balanceVoteFixture;
+
+    @Autowired
+    protected MemberFixture memberFixture;
+
+    @Autowired
+    protected RoomContentFixture roomContentFixture;
+
+    @Autowired
+    protected RoomFixture roomFixture;
 }
--- backend/src/test/java/ddangkong/service/BaseServiceTest.java
+++ backend/src/test/java/ddangkong/service/BaseServiceTest.java
@@ -3,7 +3,14 @@ package ddangkong.service;
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 
 import ddangkong.support.extension.DatabaseCleanerExtension;
+import ddangkong.support.fixture.BalanceContentFixture;
+import ddangkong.support.fixture.BalanceOptionFixture;
+import ddangkong.support.fixture.BalanceVoteFixture;
+import ddangkong.support.fixture.MemberFixture;
+import ddangkong.support.fixture.RoomContentFixture;
+import ddangkong.support.fixture.RoomFixture;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.jdbc.Sql;
 
@@ -11,4 +18,22 @@ import org.springframework.test.context.jdbc.Sql;
 @SpringBootTest(webEnvironment = WebEnvironment.NONE)
 @Sql(scripts = "/init-test.sql")
 public abstract class BaseServiceTest {
+
+    @Autowired
+    protected BalanceContentFixture balanceContentFixture;
+
+    @Autowired
+    protected BalanceOptionFixture balanceOptionFixture;
+
+    @Autowired
+    protected BalanceVoteFixture balanceVoteFixture;
+
+    @Autowired
+    protected MemberFixture memberFixture;
+
+    @Autowired
+    protected RoomContentFixture roomContentFixture;
+
+    @Autowired
+    protected RoomFixture roomFixture;
 }
