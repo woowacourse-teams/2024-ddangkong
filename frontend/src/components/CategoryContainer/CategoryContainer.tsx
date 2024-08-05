cccb0a0e401da21db439877c47e8fafcc7a18fcc
novice0840 <magnolia0840@gmail.com>
2024-08-05 16:08:38 +0900

    refactor: h2 -> span 태그로 변경 #92

--- frontend/src/components/CategoryContainer/CategoryContainer.tsx
+++ frontend/src/components/CategoryContainer/CategoryContainer.tsx
@@ -1,5 +1,3 @@
-import React from 'react';
-
 import { categoryContainerLayout, title, subtitle } from './CategoryContainer.styled';
 
 interface CategoryContainerProps {
@@ -9,7 +7,7 @@ interface CategoryContainerProps {
 const CategoryContainer = ({ category }: CategoryContainerProps) => {
   return (
     <section css={categoryContainerLayout}>
-      <h2 css={subtitle}>카테고리</h2>
+      <span css={subtitle}>카테고리</span>
       <h1 css={title}>{category}</h1>
     </section>
   );
