13c20e70aa4f9021c3c3190706c72031cb4e6865
Nam Gi Beom <rlqja0523@naver.com>
2024-08-06 22:29:46 +0900

    feat: retention policy class로 변경 #144
    Co-authored-by: leegwichan <cnddkscndgus@naver.com>

--- backend/src/main/java/ddangkong/aop/logging/Polling.java
+++ backend/src/main/java/ddangkong/aop/logging/Polling.java
@@ -6,6 +6,6 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 @Target(ElementType.METHOD)
-@Retention(RetentionPolicy.RUNTIME)
+@Retention(RetentionPolicy.CLASS)
 public @interface Polling {
 }
