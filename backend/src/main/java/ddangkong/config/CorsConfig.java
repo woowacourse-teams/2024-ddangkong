cf240d232724f43cebf4365d4572bde23aabb902
jhon3242 <rldotnr2@gmail.com>
2024-08-07 16:17:14 +0900

    fix: cors allowedOrigin 값 생성자 주입 #145

--- backend/src/main/java/ddangkong/config/CorsConfig.java
+++ backend/src/main/java/ddangkong/config/CorsConfig.java
@@ -1,5 +1,6 @@
 package ddangkong.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -8,7 +9,11 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
 
-    private final String corsOrigin = "*";
+    private final String corsOrigin;
+
+    public CorsConfig(@Value("${cors.origin}") String corsOrigin) {
+        this.corsOrigin = corsOrigin;
+    }
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
