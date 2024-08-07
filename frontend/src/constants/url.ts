7a5fe699d5bd5d505d6c462f3c0a0a414007ebc3
useon <roqkftjs@gmail.com>
2024-08-07 22:31:14 +0900

    refactor: 게임 라운드 종료 및 게임 종료 여부를 확인하는 api 주소 수정 #106

--- frontend/src/constants/url.ts
+++ frontend/src/constants/url.ts
@@ -8,7 +8,7 @@ export const API_URL = {
     `${BASE_URL}/api/balances/rooms/${roomId}/contents/${contentId}/vote-result`,
   moveNextRound: (roomId: number) => `${BASE_URL}/api/balances/rooms/${roomId}/next-round`,
   myGameStatus: (roomId: number, round: number) =>
-    `${BASE_URL}/api/balances/rooms/${roomId}?round=${round}`,
+    `${BASE_URL}/api/balances/rooms/${roomId}/round-finished?round=${round}`,
   finalResult: (roomId: number) => `${BASE_URL}/api/balances/rooms/${roomId}/final`,
   room: `${BASE_URL}/api/balances/rooms`,
   enterRoom: (roomId: number) => `${BASE_URL}/api/balances/rooms/${roomId}/members`,
@@ -21,7 +21,7 @@ export const MOCK_API_URL = {
   balanceContent: `${BASE_URL}/api/balances/rooms/:roomId/content`,
   vote: `${BASE_URL}/api/balances/rooms/:roomId/contents/:contentId/votes`,
   roundVoteResult: `${BASE_URL}/api/balances/rooms/:roomId/contents/:contentId/vote-result`,
-  myGameStatus: `${BASE_URL}/api/balances/rooms/:roomId?round=:round`,
+  myGameStatus: `${BASE_URL}/api/balances/rooms/:roomId/round-finished?round=:round`,
   moveNextRound: `${BASE_URL}/api/balances/rooms/:roomId/next-round`,
   finalResult: `${BASE_URL}/api/balances/rooms/:roomId/final`,
   room: `${BASE_URL}/api/balances/rooms`,
