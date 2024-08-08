513d4697d4d9b4edb089135239ede22f6c1d4891
useon <roqkftjs@gmail.com>
2024-08-08 13:55:19 +0900

    refactor: 복사되는 기능 로직 수정 #132

--- frontend/src/components/common/InviteModal/useClipBoard.tsx
+++ frontend/src/components/common/InviteModal/useClipBoard.tsx
@@ -3,16 +3,10 @@ import { useState } from 'react';
 const useClipBoard = () => {
   const [isCopied, setIsCopied] = useState(false);
 
-  const copyToClipboard = (text: string) => {
-    navigator.clipboard
-      .writeText(text)
-      .then(() => {
-        setIsCopied(true);
-        setTimeout(() => setIsCopied(false), 2000);
-      })
-      .catch((error) => {
-        // TODO: 에러처리
-      });
+  const copyToClipboard = async (text: string) => {
+    await navigator.clipboard.writeText(text);
+    setIsCopied(true);
+    setTimeout(() => setIsCopied(false), 2000);
   };
 
   return { isCopied, copyToClipboard };
