a7f0bebff4ed9c770a541f1bd3aaca8e787bfb96
useon <roqkftjs@gmail.com>
2024-08-08 13:38:33 +0900

    refactor: mutateAsync를 mutate로 변경하고 바로 호출하도록 수정 #106

--- frontend/src/components/common/NextRoundButton/NextRoundButton.tsx
+++ frontend/src/components/common/NextRoundButton/NextRoundButton.tsx
@@ -11,31 +11,27 @@ import { memberInfoState } from '@/recoil/atom';
 import { Theme } from '@/styles/Theme';
 
 const NextRoundButton = () => {
-  const goToNextRound = async () => {
-    await moveNextRound();
-  };
-
   const { roomId } = useParams();
   const { balanceContent } = useBalanceContentQuery();
-  const { mutateAsync: moveNextRound } = useMoveNextRoundMutation(Number(roomId));
+  const { mutate: moveNextRound } = useMoveNextRoundMutation(Number(roomId));
+  const { mutate: resetRoom } = useResetRoomMutation(Number(roomId));
   const memberInfo = useRecoilValue(memberInfoState);
   const isLastRound = balanceContent?.currentRound === balanceContent?.totalRound;
 
-  const { mutate: resetRoom } = useResetRoomMutation(Number(roomId));
   return (
     <div css={bottomButtonLayout}>
       {memberInfo.isMaster ? (
         <Button
           style={{ width: '100%' }}
           text={isLastRound ? '결과 확인' : '다음'}
-          onClick={isLastRound ? resetRoom : goToNextRound}
+          onClick={isLastRound ? resetRoom : moveNextRound}
           disabled={!memberInfo.isMaster}
         />
       ) : (
         <Button
           style={{ width: '100%', backgroundColor: Theme.color.gray }}
           text={'방장이 진행해 주세요'}
-          onClick={isLastRound ? resetRoom : goToNextRound}
+          onClick={isLastRound ? resetRoom : moveNextRound}
           disabled={!memberInfo.isMaster}
         />
       )}
