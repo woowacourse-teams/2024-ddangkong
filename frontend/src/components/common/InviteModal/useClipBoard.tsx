39f57ac850ef36064189e223f51d505bc99dd686
useon <roqkftjs@gmail.com>
2024-08-08 10:39:49 +0900

    refactor: 복사가 완료되면 state가 변경 되도록 수정 #132

--- frontend/src/components/common/InviteModal/useClipBoard.tsx
+++ frontend/src/components/common/InviteModal/useClipBoard.tsx
@@ -3,10 +3,16 @@ import { useState } from 'react';
 const useClipBoard = () => {
   const [isCopied, setIsCopied] = useState(false);
 
-  const copyToClipboard = async (text: string) => {
-    await navigator.clipboard.writeText(text);
-    setIsCopied(true);
-    setTimeout(() => setIsCopied(false), 2000);
+  const copyToClipboard = (text: string) => {
+    navigator.clipboard
+      .writeText(text)
+      .then(() => {
+        setIsCopied(true);
+        setTimeout(() => setIsCopied(false), 2000);
+      })
+      .catch((error) => {
+        // TODO: 에러처리
+      });
   };
 
   return { isCopied, copyToClipboard };
