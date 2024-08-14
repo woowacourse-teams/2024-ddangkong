c7b3ca5d2bcde388da423d4117c9d3eae8d040be
rbgksqkr <rbgks1937@naver.com>
2024-08-14 19:41:00 +0900

    test: 아무도 투표하지 않을 경우 통계 대신 다른 텍스트를 보여주는 테스트 코드 작성 #155

--- frontend/src/components/TabContentContainer/TabContentContainer.test.tsx
+++ frontend/src/components/TabContentContainer/TabContentContainer.test.tsx
@@ -1,11 +1,14 @@
 import { renderHook, screen, waitFor } from '@testing-library/react';
+import { http, HttpResponse } from 'msw';
 
 import TabContentContainer from './TabContentContainer';
 
+import { MOCK_API_URL } from '@/constants/url';
 import useRoundVoteResultQuery from '@/hooks/useRoundVoteResultQuery';
+import ROUND_VOTE_RESULT from '@/mocks/data/roundVoteResult.json';
+import { server } from '@/mocks/server';
 
 import { customRender, wrapper } from '@/test-utils';
-
 describe('TabContentContainer 컴포넌트 테스트', () => {
   it('라운드 결과 그룹원들이 선택한 퍼센트를 카운팅 애니메이션으로 보여준다.', async () => {
     customRender(<TabContentContainer isGroupTabActive={true} />);
@@ -27,6 +30,23 @@ describe('TabContentContainer 컴포넌트 테스트', () => {
     await waitFor(() => {
       expect(result.current.data?.group.firstOption.percent).toBe(73);
       expect(result.current.data?.group.secondOption.percent).toBe(27);
+    });
+  });
+
+  it('아무도 투표하지 않아 모두 기권인 경우, "아무도 투표하지 않으셨네요 :)" 텍스트를 보여준다.', async () => {
+    ROUND_VOTE_RESULT.group.firstOption.percent = 0;
+    ROUND_VOTE_RESULT.group.secondOption.percent = 0;
+
+    server.use(
+      http.get(MOCK_API_URL.roundVoteResult, () => {
+        return HttpResponse.json(ROUND_VOTE_RESULT);
+      }),
+    );
+
+    customRender(<TabContentContainer isGroupTabActive={true} />);
+
+    await waitFor(() => {
+      expect(screen.getByText('아무도 투표하지 않으셨네요 :)')).toBeInTheDocument();
     });
   });
 });
