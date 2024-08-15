6fac295ad17e35017efbd1e3b0942769f4e80fe6
useon <roqkftjs@gmail.com>
2024-08-16 00:27:04 +0900

    feat: 방장이 라운드 결과 페이지에서 버튼을 누르면 모달로 다음 라운드 진행을 안내 해주는 기능 #168

--- frontend/src/components/common/NextRoundButton/NextRoundButton.tsx
+++ frontend/src/components/common/NextRoundButton/NextRoundButton.tsx
@@ -1,18 +1,17 @@
-import { useParams } from 'react-router-dom';
 import { useRecoilValue } from 'recoil';
 
-import useMoveNextRoundMutation from './NextRoundButton.hook';
 import Button from '../Button/Button';
 import { bottomButtonLayout } from '../Button/Button.styled';
 
 import useBalanceContentQuery from '@/hooks/useBalanceContentQuery';
 import { memberInfoState } from '@/recoil/atom';
-import { Theme } from '@/styles/Theme';
 
-const NextRoundButton = () => {
-  const { roomId } = useParams();
+interface NextRoundButtonProps {
+  handleModalOpen: () => void;
+}
+
+const NextRoundButton = ({ handleModalOpen }: NextRoundButtonProps) => {
   const { balanceContent } = useBalanceContentQuery();
-  const { mutate: moveNextRound } = useMoveNextRoundMutation(Number(roomId));
   const memberInfo = useRecoilValue(memberInfoState);
   const isLastRound = balanceContent?.currentRound === balanceContent?.totalRound;
 
@@ -22,14 +21,14 @@ const NextRoundButton = () => {
         <Button
           style={{ width: '100%' }}
           text={isLastRound ? '결과 확인' : '다음'}
-          onClick={moveNextRound}
+          onClick={handleModalOpen}
           disabled={!memberInfo.isMaster}
         />
       ) : (
         <Button
-          style={{ width: '100%', backgroundColor: Theme.color.gray }}
+          style={{ width: '100%' }}
           text={'방장이 진행해 주세요'}
-          onClick={moveNextRound}
+          onClick={handleModalOpen}
           disabled={!memberInfo.isMaster}
         />
       )}
