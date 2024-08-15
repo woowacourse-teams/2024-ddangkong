98ae25520f57c389a75e3f0fee70e69101395162
Nam Gi Beom <rlqja0523@naver.com>
2024-08-15 21:45:57 +0900

    feat: RoomBalanceVote 엔티티 추가 #178

---
+++ backend/src/main/java/ddangkong/domain/room/balance/roomvote/RoomBalanceVoteRepository.java
@@ -0,0 +1,6 @@
+package ddangkong.domain.room.balance.roomvote;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoomBalanceVoteRepository extends JpaRepository<RoomBalanceVote, Long> {
+}
