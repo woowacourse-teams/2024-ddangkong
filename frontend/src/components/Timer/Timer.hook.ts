56f3fbc1eb69330579aa5d1491816bf5f6ede9fe
rbgksqkr <rbgks1937@naver.com>
2024-08-18 17:24:41 +0900

    refactor: hook이 아닌 UI 컴포넌트에서 roomId 가져오기 #198

--- frontend/src/components/Timer/Timer.hook.ts
+++ frontend/src/components/Timer/Timer.hook.ts
@@ -1,5 +1,4 @@
 import { useEffect, useRef, useState } from 'react';
-import { useParams } from 'react-router-dom';
 
 import { convertMsecToSecond } from './Timer.util';
 
@@ -9,9 +8,8 @@ const INITIAL_WIDTH = 100;
 const DELAY = 1000;
 const DEFAULT_TIME_LIMIT_MSEC = 10000;
 
-const useRoundTimer = () => {
-  const { roomId } = useParams();
-  const { balanceContent } = useBalanceContentQuery(Number(roomId));
+const useRoundTimer = (roomId: number) => {
+  const { balanceContent } = useBalanceContentQuery(roomId);
   const timeLimit = balanceContent?.timeLimit || DEFAULT_TIME_LIMIT_MSEC;
 
   const [leftRoundTime, setLeftRoundTime] = useState(convertMsecToSecond(timeLimit));
