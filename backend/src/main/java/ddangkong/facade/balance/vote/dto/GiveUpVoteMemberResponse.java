74f35d15cbb1d7ef80d748f2e03b92a756e7f8a6
PgmJun <84304802+PgmJun@users.noreply.github.com>
2024-08-20 14:31:26 +0900

    style: record 코드 컨벤션 설정 #208

--- backend/src/main/java/ddangkong/facade/balance/vote/dto/GiveUpVoteMemberResponse.java
+++ backend/src/main/java/ddangkong/facade/balance/vote/dto/GiveUpVoteMemberResponse.java
@@ -3,7 +3,9 @@ package ddangkong.facade.balance.vote.dto;
 import ddangkong.domain.room.member.Member;
 import java.util.List;
 
-public record GiveUpVoteMemberResponse(List<String> members, int memberCount) {
+public record GiveUpVoteMemberResponse(
+        List<String> members,
+        int memberCount) {
 
     public static GiveUpVoteMemberResponse create(List<Member> giveUpMembers) {
         List<String> members = giveUpMembers.stream()
