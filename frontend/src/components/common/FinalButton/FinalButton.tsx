cbb23e17addd8623f336f5dccdf53c0432abe9f4
useon <roqkftjs@gmail.com>
2024-08-22 22:31:48 +0900

    refactor: 방장만 대기 화면으로 돌아가는 버튼 활성화 되도록 수정 #236

--- frontend/src/components/common/FinalButton/FinalButton.tsx
+++ frontend/src/components/common/FinalButton/FinalButton.tsx
@@ -1,9 +1,11 @@
 import { useParams } from 'react-router-dom';
+import { useRecoilValue } from 'recoil';
 
 import Button from '../Button/Button';
 import { bottomButtonLayout } from '../Button/Button.styled';
 
 import { useResetRoomMutation } from '@/components/GameResult/GameResult.hook';
+import { memberInfoState } from '@/recoil/atom';
 
 interface FinalButtonProps {
   showModal: () => void;
@@ -12,10 +14,25 @@ interface FinalButtonProps {
 const FinalButton = ({ showModal }: FinalButtonProps) => {
   const { roomId } = useParams();
   const { mutate: resetRoom } = useResetRoomMutation(Number(roomId), showModal);
+  const memberInfo = useRecoilValue(memberInfoState);
 
   return (
     <div css={bottomButtonLayout}>
-      <Button style={{ width: '100%' }} text="확인" onClick={resetRoom} />
+      {memberInfo.isMaster ? (
+        <Button
+          style={{ width: '100%' }}
+          text="확인"
+          onClick={resetRoom}
+          disabled={!memberInfo.isMaster}
+        />
+      ) : (
+        <Button
+          style={{ width: '100%' }}
+          text="방장이 진행해 주세요"
+          onClick={resetRoom}
+          disabled={!memberInfo.isMaster}
+        />
+      )}
     </div>
   );
 };
