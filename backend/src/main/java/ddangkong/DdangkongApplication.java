51b4cecbc24aefe487ecb7851d24f5adb7a6fbcc
DESKTOP-E5VRH4Q\cnddk <cnddkscndgus@naver.com>
2024-08-22 10:18:04 +0900

    refactor: @EnableScheduling을 SpringApplication에서 분리 #209

--- backend/src/main/java/ddangkong/DdangkongApplication.java
+++ backend/src/main/java/ddangkong/DdangkongApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@EnableScheduling
 public class DdangkongApplication {
 
     public static void main(String[] args) {
