1a931ecb247d644e609debb37aa21ddaa2151ef6
rbgksqkr <rbgks1937@naver.com>
2024-08-26 11:29:46 +0900

    feat: 모든 사람에게 방장이 바뀐 정보 alert #253

--- frontend/src/pages/ReadyPage/ReadyPage.tsx
+++ frontend/src/pages/ReadyPage/ReadyPage.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 import { readyPageLayout } from './ReadyPage.styled';
 import { useGameStart } from './useGameStart';
@@ -12,15 +12,30 @@ import ReadyMembersContainer from '@/components/ReadyMembersContainer/ReadyMembe
 const ReadyPage = () => {
   const { members, roomSetting, master, isLoading, isError } = useGetRoomInfo();
   const { memberInfo, handleGameStart, setMemberInfo } = useGameStart();
+  const [prevMasterId, setPrevMasterId] = useState(master?.memberId);
 
+  // 서버 응답값을 undefined에서 갱신
+  useEffect(() => {
+    setPrevMasterId(master?.memberId);
+  }, [master?.memberId]);
+
+  // 이전 방장과 현재 방장이 다르다 -> alert -> 이전 방장값에 현재 방장 넣기
+  useEffect(() => {
+    if (prevMasterId && prevMasterId !== master?.memberId) {
+      alert(`${master?.nickname}님이 방장이 되었습니다.`);
+      setPrevMasterId(master?.memberId);
+    }
+  }, [master?.memberId]);
+
+  // 원래 방장이 아니다 + 방장의 memberId와 내 memberId가 같다 -> 방장으로 변경
   useEffect(() => {
     if (!memberInfo.isMaster && master?.memberId === memberInfo.memberId) {
       setMemberInfo({ ...memberInfo, isMaster: true });
-      alert(`${master?.nickname}님이 방장이 되었습니다.`);
     }
   }, [master?.memberId]);
 
   if (isLoading) return <Spinner imageSize={12} />;
+
   return (
     <div css={readyPageLayout}>
       <CategoryContainer category={roomSetting?.category.label} />
