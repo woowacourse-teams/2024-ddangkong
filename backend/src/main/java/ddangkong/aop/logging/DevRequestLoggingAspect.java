7dae1f08029c4cdcf18f3a36c74b8495baff9796
PgmJun <84304802+PgmJun@users.noreply.github.com>
2024-09-01 20:26:41 +0900

    refactor: 상위 클래스에 이미 존재하는 애노테이션 제거 #251

--- backend/src/main/java/ddangkong/aop/logging/DevRequestLoggingAspect.java
+++ backend/src/main/java/ddangkong/aop/logging/DevRequestLoggingAspect.java
@@ -1,14 +1,12 @@
 package ddangkong.aop.logging;
 
 
-import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
-@Slf4j
 @Aspect
 @Component
 @Profile({"dev", "local"})
