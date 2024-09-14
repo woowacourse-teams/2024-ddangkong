bbd04ff31b6e17f0238eeb134039b0fc76c6e7f7
rbgksqkr <rbgks1937@naver.com>
2024-09-15 01:35:13 +0900

    test: 타이머가 종료되었을 때 선택된 옵션이 있으면 투표 테스트 코드 작성 #261

--- frontend/src/components/Timer/Timer.test.tsx
+++ frontend/src/components/Timer/Timer.test.tsx
@@ -1,3 +1,6 @@
+import { act, renderHook } from '@testing-library/react';
+
+import useTimer from './hooks/useTimer';
 import { convertMsecToSecond, formatLeftRoundTime } from './Timer.util';
 
 describe('Timer 테스트', () => {
@@ -12,6 +15,42 @@ describe('Timer 테스트', () => {
       const convertedTime = convertMsecToSecond(10000);
 
       expect(convertedTime).toBe(10);
+    });
+  });
+  describe('Timer 훅 테스트', () => {
+    jest.useFakeTimers();
+    const voteMock = jest.fn();
+    const timeLimit = 10000;
+
+    it('타이머가 종료되었을 때 선택 완료를 누르지 않아도 선택된 옵션이 있으면 투표한다.', () => {
+      const isSelectedOption = true;
+
+      const { result } = renderHook(() =>
+        useTimer({ timeLimit, isSelectedOption, vote: voteMock }),
+      );
+
+      // act : 인자로 받은 함수를 실행시켜서 가상의 DOM(jsdom)에 적용하는 역할
+      // 상태 변경과 그로 인한 DOM 업데이트가 모두 완료된 후에 테스트가 실행되도록 보장
+      act(() => {
+        jest.advanceTimersByTime(timeLimit);
+      });
+
+      expect(result.current.leftRoundTime).toBe(0);
+      expect(voteMock).toHaveBeenCalled();
+    });
+    it('타이머가 종료되었을 때 선택된 옵션이 없다면 투표되지 않고 기권한다.', () => {
+      const isSelectedOption = false;
+
+      const { result } = renderHook(() =>
+        useTimer({ timeLimit, isSelectedOption, vote: voteMock }),
+      );
+
+      act(() => {
+        jest.advanceTimersByTime(timeLimit);
+      });
+
+      expect(result.current.leftRoundTime).toBe(0);
+      expect(voteMock).not.toHaveBeenCalled();
     });
   });
 });
