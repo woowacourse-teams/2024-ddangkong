a2580849d2aa51c02984e61fef02d66a4759f60f
rbgksqkr <rbgks1937@naver.com>
2024-09-21 15:50:32 +0900

    refactor: 이미지 형식 webp로 변환 및 이미지 크기 조절 #278

--- frontend/webpack.config.common.js
+++ frontend/webpack.config.common.js
@@ -30,7 +30,7 @@ module.exports = {
         ],
       },
       {
-        test: /\.(png|jpe?g|gif|svg)$/i,
+        test: /\.(png|jpe?g|gif|svg|webp)$/i,
         use: [
           {
             loader: 'file-loader',
