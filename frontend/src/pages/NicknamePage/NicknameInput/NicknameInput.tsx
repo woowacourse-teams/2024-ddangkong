07629ddcedabbbdf1703efb8f7feb9b53aada234
novice0840 <magnolia0840@gmail.com>
2024-09-23 16:54:41 +0900

    feat: random nickname 컴포넌트 외부에서 생성 #274

--- frontend/src/pages/NicknamePage/NicknameInput/NicknameInput.tsx
+++ frontend/src/pages/NicknamePage/NicknameInput/NicknameInput.tsx
@@ -1,4 +1,4 @@
-import { RefObject } from 'react';
+import { RefObject, memo } from 'react';
 
 import useNicknameInput from './hooks/useNicknameInput';
 import { nicknameInput, nicknameInputContainer, nicknameLengthText } from './NicknameInput.styled';
@@ -12,36 +12,35 @@ interface NicknameInputProps {
   onFocus?: () => void;
   onBlur?: () => void;
 }
+const randomNickname = createRandomNickname();
 
-const NicknameInput = ({
-  nicknameInputRef,
-  handleMakeOrEnterRoom,
-  onFocus,
-  onBlur,
-}: NicknameInputProps) => {
-  const { nickname, handleChangeInput, handleKeyDown } = useNicknameInput({
-    handleMakeOrEnterRoom,
-  });
-  const randomNickname = createRandomNickname();
+const NicknameInput = memo(
+  ({ nicknameInputRef, handleMakeOrEnterRoom, onFocus, onBlur }: NicknameInputProps) => {
+    const { nickname, handleChangeInput, handleKeyDown } = useNicknameInput({
+      handleMakeOrEnterRoom,
+    });
 
-  return (
-    <div css={nicknameInputContainer}>
-      <input
-        ref={nicknameInputRef}
-        css={nicknameInput}
-        placeholder={randomNickname}
-        maxLength={NICKNAME_MAX_LENGTH}
-        value={nickname}
-        onChange={handleChangeInput}
-        onKeyDown={handleKeyDown}
-        onFocus={onFocus}
-        onBlur={onBlur}
-      />
-      <span css={nicknameLengthText}>
-        {nickname.length}/{NICKNAME_MAX_LENGTH}
-      </span>
-    </div>
-  );
-};
+    return (
+      <div css={nicknameInputContainer}>
+        <input
+          ref={nicknameInputRef}
+          css={nicknameInput}
+          placeholder={randomNickname}
+          maxLength={NICKNAME_MAX_LENGTH}
+          value={nickname}
+          onChange={handleChangeInput}
+          onKeyDown={handleKeyDown}
+          onFocus={onFocus}
+          onBlur={onBlur}
+        />
+        <span css={nicknameLengthText}>
+          {nickname.length}/{NICKNAME_MAX_LENGTH}
+        </span>
+      </div>
+    );
+  },
+);
+
+NicknameInput.displayName = 'NicknameInput';
 
 export default NicknameInput;
