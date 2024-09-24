2c92f88a147741ef497f3524862be35b946c2498
PgmJun <84304802+PgmJun@users.noreply.github.com>
2024-09-24 16:59:58 +0900

    fix: Source DB 라우팅 네이밍 오류 수정 #287

--- backend/src/main/java/ddangkong/config/database/RoutingDataSource.java
+++ backend/src/main/java/ddangkong/config/database/RoutingDataSource.java
@@ -1,9 +1,11 @@
 package ddangkong.config.database;
 
 import java.util.List;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.jdbc.datasource.lookup.AbstractRoutingDataSource;
 import org.springframework.transaction.support.TransactionSynchronizationManager;
 
+@Slf4j
 public class RoutingDataSource extends AbstractRoutingDataSource {
 
     private final RoutingReplicas<String> routingReplicas;
@@ -16,9 +18,11 @@ public class RoutingDataSource extends AbstractRoutingDataSource {
     protected Object determineCurrentLookupKey() {
         boolean isReadOnly = TransactionSynchronizationManager.isCurrentTransactionReadOnly();
         if (isReadOnly) {
+            log.info("Routing to Replica DB server");
             return routingReplicas.get();
         } else {
-            return "source";
+            log.info("Routing to Master DB server");
+            return "master";
         }
     }
 }
