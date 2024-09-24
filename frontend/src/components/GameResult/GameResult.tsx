987f79170852933434d7154580902a81f3baa203
useon <roqkftjs@gmail.com>
2024-09-24 16:58:42 +0900

    feat: 매칭 결과가 정해진 크기 이상인 경우 스크롤, 플로팅 버튼으로 위 아래 이동 할 수 있는 기능 #273

--- frontend/src/components/GameResult/GameResult.tsx
+++ frontend/src/components/GameResult/GameResult.tsx
@@ -1,3 +1,5 @@
+import { useState, useEffect, useRef } from 'react';
+
 import { useMatchingResultQuery } from './GameResult.hook';
 import {
   gameResultLayout,
@@ -5,12 +7,15 @@ import {
   noMatchingLayout,
   noMatchingImg,
   noMatchingText,
+  floatingButton,
 } from './GameResult.styled';
 import AlertModal from '../common/AlertModal/AlertModal';
 import FinalButton from '../common/FinalButton/FinalButton';
 import Spinner from '../common/Spinner/Spinner';
 import GameResultItem from '../GameResultItem/GameResultItem';
 
+import ArrowDown from '@/assets/images/arrowDown.svg';
+import ArrowUp from '@/assets/images/arrowUp.svg';
 import SadDdangKong from '@/assets/images/sadDdangkong.png';
 import useModal from '@/hooks/useModal';
 
@@ -18,9 +23,67 @@ const GameResult = () => {
   const { isOpen, show, close } = useModal();
   const { matchedMembers, existMatching, isLoading } = useMatchingResultQuery();
 
+  const [isAtTop, setIsAtTop] = useState(true);
+  const [isAtBottom, setIsAtBottom] = useState(false);
+  const resultContainerRef = useRef<HTMLDivElement | null>(null);
+
+  useEffect(() => {
+    const container = resultContainerRef.current;
+    if (!container) return;
+
+    setTimeout(() => {
+      const { scrollHeight, clientHeight } = container;
+
+      if (scrollHeight > clientHeight) {
+        setIsAtBottom(false);
+      } else {
+        setIsAtBottom(true);
+      }
+    }, 100);
+  }, [matchedMembers]);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const container = resultContainerRef.current;
+      if (!container) return;
+
+      const { scrollTop, scrollHeight, clientHeight } = container;
+      setIsAtTop(scrollTop === 0);
+
+      setIsAtBottom(scrollTop + clientHeight >= scrollHeight - 1);
+    };
+
+    const container = resultContainerRef.current;
+    if (container) {
+      container.addEventListener('scroll', handleScroll);
+      handleScroll();
+    }
+
+    return () => {
+      if (container) {
+        container.removeEventListener('scroll', handleScroll);
+      }
+    };
+  }, []);
+
+  const scrollToTop = () => {
+    if (resultContainerRef.current) {
+      resultContainerRef.current.scrollTo({ top: 0, behavior: 'smooth' });
+    }
+  };
+
+  const scrollToBottom = () => {
+    if (resultContainerRef.current) {
+      resultContainerRef.current.scrollTo({
+        top: resultContainerRef.current.scrollHeight,
+        behavior: 'smooth',
+      });
+    }
+  };
+
   return (
     <>
-      <div css={gameResultLayout}>
+      <div css={gameResultLayout} ref={resultContainerRef}>
         {isLoading && <Spinner message="매칭 결과를 불러오는 중입니다..." />}
 
         {existMatching && (
@@ -42,6 +105,17 @@ const GameResult = () => {
               {'이번에는 나와 같은 선택을 한 사람이 없지만,\n다음 게임을 기대해 볼까요?'}
             </span>
           </div>
+        )}
+
+        {!isAtTop && (
+          <button onClick={scrollToTop} css={floatingButton('up')}>
+            <img src={ArrowUp} alt="" />
+          </button>
+        )}
+        {!isAtBottom && (
+          <button onClick={scrollToBottom} css={floatingButton('down')}>
+            <img src={ArrowDown} alt="" />
+          </button>
         )}
       </div>
       <FinalButton showModal={show} />
