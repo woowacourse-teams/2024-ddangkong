1af176ddc89379f0915a3899c260df2c59b5b6f2
useon <roqkftjs@gmail.com>
2024-09-26 03:32:20 +0900

    refactor: RoundVoteContainer 테스트 코드 수정 #290

--- frontend/src/components/RoundVoteContainer/RoundVoteContainer.test.tsx
+++ frontend/src/components/RoundVoteContainer/RoundVoteContainer.test.tsx
@@ -6,18 +6,33 @@ import RoundVoteContainer from './RoundVoteContainer';
 import { customRender } from '@/test-utils';
 
 describe('RoundVoteContainer 컴포넌트 테스트', () => {
-  it('기본 탭인 그룹 탭에서, 전체 탭을 클릭하면 전체 사용자 통계를 퍼센트로 보여준다.', async () => {
+  it('기본 탭인 투표 통계 탭에서, 투표 현황 탭을 클릭하면 투표 현황을 보여준다.', async () => {
     const user = userEvent.setup();
     customRender(<RoundVoteContainer />);
 
-    const button = await screen.findByRole('button', { name: '전체' });
-
+    const button = await screen.findByRole('button', { name: '투표 현황' });
     await user.click(button);
 
+    screen.debug();
     await waitFor(
       () => {
-        expect(screen.getByText('16%')).toBeInTheDocument();
-        expect(screen.getByText('84%')).toBeInTheDocument();
+        // 첫 번째 선택지와 투표자 확인
+        expect(
+          screen.getByText((content) => content.includes('100억 빚 송강')),
+        ).toBeInTheDocument();
+        expect(screen.getByText((content) => content.includes('d'))).toBeInTheDocument();
+        // 두 번째 선택지와 투표자 확인
+        expect(
+          screen.getByText((content) => content.includes('100억 부자 송강호')),
+        ).toBeInTheDocument();
+        expect(
+          screen.getByText((content) => content.includes('일이삼사오육칠팔구십일이')),
+        ).toBeInTheDocument();
+        // 기권자 확인
+        expect(
+          screen.getByText((content) => content.includes('투표에 참여하지 않으셨어요')),
+        ).toBeInTheDocument();
+        expect(screen.getByText((content) => content.includes('ㅁ'))).toBeInTheDocument();
       },
       { timeout: 3000 },
     );
