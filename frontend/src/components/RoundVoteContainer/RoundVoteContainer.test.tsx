d7fa617f304540c331e7655e1dfdf5a969797fea
useon <roqkftjs@gmail.com>
2024-09-26 16:35:05 +0900

    refactor: 불 필요한 타임 아웃 코드 삭제 #290

--- frontend/src/components/RoundVoteContainer/RoundVoteContainer.test.tsx
+++ frontend/src/components/RoundVoteContainer/RoundVoteContainer.test.tsx
@@ -13,27 +13,22 @@ describe('RoundVoteContainer 컴포넌트 테스트', () => {
     const button = await screen.findByRole('button', { name: '투표 현황' });
     await user.click(button);
 
-    await waitFor(
-      () => {
-        // 첫 번째 선택지와 투표자 확인
-        expect(
-          screen.getByText((content) => content.includes('100억 빚 송강')),
-        ).toBeInTheDocument();
-        expect(screen.getByText((content) => content.includes('d'))).toBeInTheDocument();
-        // 두 번째 선택지와 투표자 확인
-        expect(
-          screen.getByText((content) => content.includes('100억 부자 송강호')),
-        ).toBeInTheDocument();
-        expect(
-          screen.getByText((content) => content.includes('일이삼사오육칠팔구십일이')),
-        ).toBeInTheDocument();
-        // 기권자 확인
-        expect(
-          screen.getByText((content) => content.includes('투표에 참여하지 않으셨어요')),
-        ).toBeInTheDocument();
-        expect(screen.getByText((content) => content.includes('ㅁ'))).toBeInTheDocument();
-      },
-      { timeout: 3000 },
-    );
+    await waitFor(() => {
+      // 첫 번째 선택지와 투표자 확인
+      expect(screen.getByText((content) => content.includes('100억 빚 송강'))).toBeInTheDocument();
+      expect(screen.getByText((content) => content.includes('d'))).toBeInTheDocument();
+      // 두 번째 선택지와 투표자 확인
+      expect(
+        screen.getByText((content) => content.includes('100억 부자 송강호')),
+      ).toBeInTheDocument();
+      expect(
+        screen.getByText((content) => content.includes('일이삼사오육칠팔구십일이')),
+      ).toBeInTheDocument();
+      // 기권자 확인
+      expect(
+        screen.getByText((content) => content.includes('투표에 참여하지 않으셨어요')),
+      ).toBeInTheDocument();
+      expect(screen.getByText((content) => content.includes('ㅁ'))).toBeInTheDocument();
+    });
   });
 });
