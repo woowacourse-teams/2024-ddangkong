31d809af3d0eb8309136ac40000a319061189800
useon <roqkftjs@gmail.com>
2024-10-07 22:00:58 +0900

    refactor: 테스트 코드의 상수화를 통해 코드 가독성 개선 #319

--- frontend/src/components/common/a11yOnly/A11yOnly.test.tsx
+++ frontend/src/components/common/a11yOnly/A11yOnly.test.tsx
@@ -3,22 +3,17 @@ import { render, screen } from '@testing-library/react';
 import AllyOnly from './A11yOnly';
 
 describe('A11yOnly 컴포넌트 테스트', () => {
+  const SCREEN_READER_TEXT = '화면에 표시되지 않고 스크린 리더로 읽을 수 있습니다.';
   test('스크린 리더 전용 텍스트가 DOM에 존재하는지 확인한다.', () => {
-    render(<AllyOnly>화면에 표시되지 않고 스크린 리더로 읽을 수 있습니다.</AllyOnly>);
+    render(<AllyOnly>{SCREEN_READER_TEXT}</AllyOnly>);
 
-    const screenReaderElement = screen.getByText(
-      '화면에 표시되지 않고 스크린 리더로 읽을 수 있습니다.',
-    );
+    const screenReaderElement = screen.getByText(SCREEN_READER_TEXT);
 
     expect(screenReaderElement).toBeInTheDocument();
   });
 
   test('aria-label 속성이 적용된 텍스트가 스크린 리더로 읽히는지 확인한다.', () => {
-    render(
-      <AllyOnly aria-label="화면에 표시되지 않고 스크린 리더로 읽을 수 있습니다.">
-        안녕하세요.
-      </AllyOnly>,
-    );
+    render(<AllyOnly aria-label={SCREEN_READER_TEXT}>안녕하세요.</AllyOnly>);
 
     const screenReaderElement = screen.getByLabelText(
       '화면에 표시되지 않고 스크린 리더로 읽을 수 있습니다.',
@@ -30,11 +25,11 @@ describe('A11yOnly 컴포넌트 테스트', () => {
   test('aria-live 속성이 적용된 실시간 업데이트 메시지가 스크린 리더에 의해 읽히는지 확인한다.', () => {
     render(
       <AllyOnly as="div" aria-live="polite">
-        실시간 업데이트 메시지
+        {SCREEN_READER_TEXT}
       </AllyOnly>,
     );
 
-    const liveUpdateMessage = screen.getByText('실시간 업데이트 메시지');
+    const liveUpdateMessage = screen.getByText(SCREEN_READER_TEXT);
 
     expect(liveUpdateMessage).toHaveAttribute('aria-live', 'polite');
   });
