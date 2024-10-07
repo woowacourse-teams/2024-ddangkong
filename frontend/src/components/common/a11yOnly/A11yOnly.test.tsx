e8d108b85292ec4545ac5746e2c325373e6ad5bc
useon <roqkftjs@gmail.com>
2024-10-07 22:10:09 +0900

    test: 컴포넌트의 as 속성을 테스트하는 테스트 케이스 추가 #319

--- frontend/src/components/common/a11yOnly/A11yOnly.test.tsx
+++ frontend/src/components/common/a11yOnly/A11yOnly.test.tsx
@@ -23,14 +23,18 @@ describe('A11yOnly 컴포넌트 테스트', () => {
   });
 
   test('aria-live 속성이 적용된 실시간 업데이트 메시지가 스크린 리더에 의해 읽히는지 확인한다.', () => {
-    render(
-      <AllyOnly as="div" aria-live="polite">
-        {SCREEN_READER_TEXT}
-      </AllyOnly>,
-    );
+    render(<AllyOnly aria-live="polite">{SCREEN_READER_TEXT}</AllyOnly>);
 
     const liveUpdateMessage = screen.getByText(SCREEN_READER_TEXT);
 
     expect(liveUpdateMessage).toHaveAttribute('aria-live', 'polite');
+  });
+
+  test('as 속성을 사용하여 p 태그로 렌더링되는지 확인한다.', () => {
+    render(<AllyOnly as="p">{SCREEN_READER_TEXT}</AllyOnly>);
+
+    const screenReaderElement = screen.getByText(SCREEN_READER_TEXT);
+
+    expect(screenReaderElement.tagName.toLowerCase()).toBe('p');
   });
 });
