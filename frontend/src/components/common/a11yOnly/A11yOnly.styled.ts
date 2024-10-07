0fb29afc835ac905a0b7f8ca925c5ff386d263e0
useon <roqkftjs@gmail.com>
2024-10-07 20:51:43 +0900

    refactor: 컨벤션에 맞게 a11yOnly 컴포넌트 및 스타일 수정 #319

--- frontend/src/components/common/a11yOnly/A11yOnly.styled.ts
+++ frontend/src/components/common/a11yOnly/A11yOnly.styled.ts
@@ -1,13 +1,14 @@
 import { css } from '@emotion/react';
 
-export const a11yOnlyContainer = css({
-  position: 'absolute',
-  width: '1px',
-  height: '1px',
-  padding: 0,
-  margin: '-1px',
-  border: 0,
-  clip: 'rect(0, 0, 0, 0)',
-  overflow: 'hidden',
-  whiteSpace: 'nowrap',
-});
+export const a11yOnlyLayout = css`
+  overflow: hidden;
+  position: absolute;
+  width: 1px;
+  height: 1px;
+  margin: -1px;
+  padding: 0;
+  border: 0;
+
+  white-space: nowrap;
+  clip: rect(0, 0, 0, 0);
+`;
--- frontend/src/components/common/a11yOnly/A11yOnly.tsx
+++ frontend/src/components/common/a11yOnly/A11yOnly.tsx
@@ -1,6 +1,6 @@
 import { ElementType, ReactNode } from 'react';
 
-import { a11yOnlyContainer } from './A11yOnly.styled';
+import { a11yOnlyLayout } from './A11yOnly.styled';
 
 interface A11yOnlyProps<T extends ElementType = 'span'> {
   as?: T;
@@ -10,7 +10,7 @@ interface A11yOnlyProps<T extends ElementType = 'span'> {
 const A11yOnly = <T extends ElementType = 'span'>({ as, children, ...props }: A11yOnlyProps<T>) => {
   const Component = as || 'span';
   return (
-    <Component {...props} css={a11yOnlyContainer}>
+    <Component {...props} css={a11yOnlyLayout}>
       {children}
     </Component>
   );
