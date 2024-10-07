3c96ddc1ac601e7cc03c887f392a7a6fe1773ca7
useon <roqkftjs@gmail.com>
2024-10-07 21:53:56 +0900

    refactor: A11yOnly컴포넌트 타입을 ComponentPropsWithoutRef로 수정하고 children을 명시적으로 받지 않도록 개선 #319

--- frontend/src/components/common/a11yOnly/A11yOnly.tsx
+++ frontend/src/components/common/a11yOnly/A11yOnly.tsx
@@ -1,19 +1,17 @@
-import { ElementType, ReactNode } from 'react';
+import { ElementType, ComponentPropsWithoutRef } from 'react';
 
 import { a11yOnlyLayout } from './A11yOnly.styled';
 
 interface A11yOnlyProps<T extends ElementType = 'span'> {
   as?: T;
-  children: ReactNode;
 }
 
-const A11yOnly = <T extends ElementType = 'span'>({ as, children, ...props }: A11yOnlyProps<T>) => {
+const A11yOnly = <T extends ElementType = 'span'>({
+  as,
+  ...props
+}: A11yOnlyProps<T> & ComponentPropsWithoutRef<T>) => {
   const Component = as || 'span';
-  return (
-    <Component {...props} css={a11yOnlyLayout}>
-      {children}
-    </Component>
-  );
+  return <Component {...props} css={a11yOnlyLayout} />;
 };
 
 export default A11yOnly;
