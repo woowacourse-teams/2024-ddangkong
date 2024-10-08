da701249ef173ff82282f0f46888136477fbabd2
useon <roqkftjs@gmail.com>
2024-10-08 17:13:19 +0900

    refactor: PropsWithChildren을 사용하여 children이 자동으로 포함되도록 수정 #319

--- frontend/src/components/common/a11yOnly/A11yOnly.tsx
+++ frontend/src/components/common/a11yOnly/A11yOnly.tsx
@@ -1,23 +1,21 @@
-import { ElementType, AriaRole, ReactNode } from 'react';
+import { ElementType, AriaRole, PropsWithChildren } from 'react';
 
 import { a11yOnlyLayout } from './A11yOnly.styled';
 
 interface A11yOnlyProps<T extends ElementType = 'span'> {
-  children: ReactNode;
   as?: T;
   role?: AriaRole;
 }
 
 const A11yOnly = <T extends ElementType = 'span'>({
-  children,
   as,
   role = 'text',
   ...props
-}: A11yOnlyProps<T>) => {
+}: PropsWithChildren<A11yOnlyProps<T>>) => {
   const Component = as || 'span';
   return (
     <Component css={a11yOnlyLayout} role={role} {...props}>
-      {children}
+      {props.children}
     </Component>
   );
 };
