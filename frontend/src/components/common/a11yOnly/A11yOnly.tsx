99c9b6596cdf4006c396adc5d8943cfcd4252e95
useon <roqkftjs@gmail.com>
2024-10-08 16:05:20 +0900

    refactor: role 타입을 받도록 속성 추가 및 children 명시적으로 받도록 수정 #319

--- frontend/src/components/common/a11yOnly/A11yOnly.tsx
+++ frontend/src/components/common/a11yOnly/A11yOnly.tsx
@@ -1,17 +1,25 @@
-import { ElementType, ComponentPropsWithoutRef } from 'react';
+import { ElementType, AriaRole, ReactNode } from 'react';
 
 import { a11yOnlyLayout } from './A11yOnly.styled';
 
 interface A11yOnlyProps<T extends ElementType = 'span'> {
+  children: ReactNode;
   as?: T;
+  role?: AriaRole;
 }
 
 const A11yOnly = <T extends ElementType = 'span'>({
+  children,
   as,
+  role = 'text',
   ...props
-}: A11yOnlyProps<T> & ComponentPropsWithoutRef<T>) => {
+}: A11yOnlyProps<T>) => {
   const Component = as || 'span';
-  return <Component {...props} css={a11yOnlyLayout} />;
+  return (
+    <Component css={a11yOnlyLayout} role={role} {...props}>
+      {children}
+    </Component>
+  );
 };
 
 export default A11yOnly;
