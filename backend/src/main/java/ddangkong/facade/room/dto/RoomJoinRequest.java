ce5c1e7ef7cb21b17340a0e26327ecbbd7b7bab6
jhon3242 <rldotnr2@gmail.com>
2024-10-10 10:58:10 +0900

    feat: DTO 최대 문자열 길이 검증 삭제 #314

--- backend/src/main/java/ddangkong/facade/room/dto/RoomJoinRequest.java
+++ backend/src/main/java/ddangkong/facade/room/dto/RoomJoinRequest.java
@@ -1,11 +1,9 @@
 package ddangkong.facade.room.dto;
 
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Size;
 
 public record RoomJoinRequest(
         @NotBlank
-        @Size(max = 255)
         String nickname
 ) {
 }
