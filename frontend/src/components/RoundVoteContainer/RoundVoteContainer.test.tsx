751f2835c9b9007af77f4d44ccc9011348b88292
useon <roqkftjs@gmail.com>
2024-10-13 18:03:23 +0900

    refactor: mock data를 이용하도록 테스트 수정 및 투표를 하지 않은 멤버 확인 테스트 추가 #325

--- frontend/src/components/RoundVoteContainer/RoundVoteContainer.test.tsx
+++ frontend/src/components/RoundVoteContainer/RoundVoteContainer.test.tsx
@@ -3,6 +3,8 @@ import { userEvent } from '@testing-library/user-event';
 
 import RoundVoteContainer from './RoundVoteContainer';
 
+import ROUND_VOTE_RESULT from '@/mocks/data/roundVoteResult.json';
+
 import { customRender } from '@/test-utils';
 
 describe('RoundVoteContainer 컴포넌트 테스트', () => {
@@ -14,16 +16,23 @@ describe('RoundVoteContainer 컴포넌트 테스트', () => {
     await user.click(button);
 
     await waitFor(() => {
-      // 첫 번째 선택지의 투표자 확인
-      expect(screen.getByText('d')).toBeInTheDocument();
+      // 첫 번째 선택지의 투표 멤버 확인
+      const firstOptionMembers = ROUND_VOTE_RESULT.group.firstOption.members;
+      firstOptionMembers.forEach((member) => {
+        expect(screen.getByText(member)).toBeInTheDocument();
+      });
 
-      // 두 번째 선택지의 투표자들 확인
-      expect(screen.getByText('일이삼사오육칠팔구십일이')).toBeInTheDocument();
-      expect(screen.getByText('가나다라마바사아자차카타')).toBeInTheDocument();
-      expect(screen.getByText('abc')).toBeInTheDocument();
-      expect(screen.getByText('땅콩땅콩땅콩땅콩땅콩땅콩')).toBeInTheDocument();
-      expect(screen.getByText('123456789012')).toBeInTheDocument();
-      expect(screen.getByText('안녕하세요안녕하세요안녕')).toBeInTheDocument();
+      // 두 번째 선택지의 투표 멤버 확인
+      const secondOptionMembers = ROUND_VOTE_RESULT.group.secondOption.members;
+      secondOptionMembers.forEach((member) => {
+        expect(screen.getByText(member)).toBeInTheDocument();
+      });
+
+      // 투표를 하지 않은 멤버 확인
+      const giveUpMembers = ROUND_VOTE_RESULT.group.giveUp.members;
+      giveUpMembers.forEach((member) => {
+        expect(screen.getByText(member)).toBeInTheDocument();
+      });
     });
   });
 });
