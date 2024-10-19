1d4352c7fdb9ddbe46bc0a36e1882e1f9b9a33bf
rbgksqkr <rbgks1937@naver.com>
2024-10-19 14:42:59 +0900

    fix: 홈화면 외에 페이지에서 새로고침 후 페이지 이동시 발생하는 비동기 에러 해결 #340

--- frontend/src/router/index.tsx
+++ frontend/src/router/index.tsx
@@ -42,7 +42,7 @@ export const router = createBrowserRouter([
             path: 'nickname/:roomUuid?',
             element: (
               <AsyncErrorBoundary>
-                <NicknamePage />,
+                <NicknamePage />
               </AsyncErrorBoundary>
             ),
           },
