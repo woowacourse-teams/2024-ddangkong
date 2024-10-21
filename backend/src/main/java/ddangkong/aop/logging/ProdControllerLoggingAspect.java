6b84f0c29545182ab839ca6420235beb48052f6e
PgmJun <84304802+PgmJun@users.noreply.github.com>
2024-10-21 14:32:56 +0900

    chore: Prod 환경도 Response 정보 로깅하도록 구성 #355

--- backend/src/main/java/ddangkong/aop/logging/ProdControllerLoggingAspect.java
+++ backend/src/main/java/ddangkong/aop/logging/ProdControllerLoggingAspect.java
@@ -2,6 +2,7 @@ package ddangkong.aop.logging;
 
 
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.context.annotation.Profile;
@@ -15,5 +16,10 @@ public class ProdControllerLoggingAspect extends ControllerLoggingAspect {
     @Before("allControllerWithoutPolling()")
     public void logControllerRequest(JoinPoint joinPoint) {
         super.logControllerRequest(joinPoint);
+    }
+
+    @AfterReturning(pointcut = "allControllerWithoutPolling()", returning = "responseBody")
+    protected void logControllerResponse(JoinPoint joinPoint, Object responseBody) {
+        super.logControllerResponse(joinPoint, responseBody);
     }
 }
