033140a5ec67c3e760bcf58d694c7a726e458bbb
Nam Gi Beom <rlqja0523@naver.com>
2024-10-23 14:41:00 +0900

    fix: 무중단 배포 github action v1.12 #327

--- backend/scripts/get_blue_green_port.sh
+++ backend/scripts/get_blue_green_port.sh
@@ -2,10 +2,8 @@
 
 set -e
 
-NGINX_CONFIG="/etc/nginx/sites-available/default"
-BLUE_PORT=""
-
 # 현재 BLUE 호스트 확인 (8080 or 8081)
+NGINX_CONFIG="/etc/nginx/sites-available/default"
 BLUE_PORT=$(sudo grep -oP '127\.0\.0\.1:\K(8080|8081)' "$NGINX_CONFIG")
 
 # 오류 처리: BLUE 호스트를 찾을 수 없을 경우
@@ -17,9 +15,10 @@ fi
 
 # 환경 변수 설정
 if [ "$BLUE_PORT" = "8080" ]; then
-    export BLUE_PORT="8080"
-    export GREEN_PORT="8081"
+
+    echo "blue_port=8080"
+    echo "green_port=8081"
 else
-    export BLUE_PORT="8081"
-    export GREEN_PORT="8080"
+    echo "blue_port=8081"
+    echo "green_port=8080"
 fi
