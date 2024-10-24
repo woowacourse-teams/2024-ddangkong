ee62f6d515f1054c7567c5133b9be0f9bd0757c8
Nam Gi Beom <rlqja0523@naver.com>
2024-10-24 14:36:26 +0900

    fix: 무중단 배포 github action v1.36 #327

--- backend/scripts/change_nginx_port_forwarding.sh
+++ backend/scripts/change_nginx_port_forwarding.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
 NGINX_CONFIG="/etc/nginx/sites-available/default"
-PORT=$1
+OLD_PORT=$1
+NEW_PORT=$2
 
-CURRENT_PORT=$(sudo grep -oP '127.0.0.1:\K\d+' "$NGINX_CONFIG")
+if [ -z "$OLD_PORT" ] || [ -z "$NEW_PORT" ]; then
+    echo "Invalid arguments: OLD_PORT=$OLD_PORT, NEW_PORT=$NEW_PORT"
+    exit 1
+fi
 
-if [ -n "$CURRENT_PORT" ]; then
-    sudo sed -i "s/127.0.0.1:$CURRENT_PORT/127.0.0.1:$PORT/g" "$NGINX_CONFIG"
-    echo "Switching from ($CURRENT_PORT) to ($PORT)"
+if sudo grep -q "127\.0\.0\.1:$OLD_PORT" "$NGINX_CONFIG"; then
+    sudo sed -i "s/127.0.0.1:$OLD_PORT/127.0.0.1:$NEW_PORT/g" "$NGINX_CONFIG"
+    echo "Switching from $OLD_PORT to $NEW_PORT"
 else
     echo "Can't find port forwarding format in nginx configuration"
     exit 1
