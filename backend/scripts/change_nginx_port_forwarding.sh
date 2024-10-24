f74e6ccba4bdd7e362ea371168fb1fdf14ac3694
Nam Gi Beom <rlqja0523@naver.com>
2024-10-24 15:33:18 +0900

    fix: 무중단 배포 github action v1.43 #327

--- backend/scripts/change_nginx_port_forwarding.sh
+++ backend/scripts/change_nginx_port_forwarding.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
 NGINX_CONFIG="/etc/nginx/sites-available/default"
-OLD_PORT=$1
-NEW_PORT=$2
+NEW_PORT=$1
 
-if [ -z "$OLD_PORT" ] || [ -z "$NEW_PORT" ]; then
-    echo "Invalid arguments: OLD_PORT=$OLD_PORT, NEW_PORT=$NEW_PORT"
+if [ -z "$NEW_PORT" ]; then
+    echo "Invalid new port : $NEW_PORT"
     exit 1
 fi
 
-if sudo grep -q "127\.0\.0\.1:$OLD_PORT" "$NGINX_CONFIG"; then
+OLD_PORT=$(sudo grep -oP '127.0.0.1:\K\d+' "$NGINX_CONFIG")
+
+if  [ -n "$OLD_PORT" ]; then
     sudo sed -i "s/127.0.0.1:$OLD_PORT/127.0.0.1:$NEW_PORT/g" "$NGINX_CONFIG"
     echo "Switching from $OLD_PORT to $NEW_PORT"
 else
