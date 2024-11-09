f226ddd9ea14bf76887ee65a3eb559d222a05722
rbgksqkr <rbgks1937@naver.com>
2024-11-09 21:42:54 +0900

    refactor: MainPage 훅 디렉토리 수정 #276

--- frontend/src/pages/MainPage/hooks/useCreateRoom.ts
+++ frontend/src/pages/MainPage/hooks/useCreateRoom.ts
@@ -2,11 +2,14 @@ import { useNavigate } from 'react-router-dom';
 
 import { ROUTES } from '@/constants/routes';
 
-export const useCreateRoom = () => {
+const useCreateRoom = () => {
   const navigate = useNavigate();
+
   const handleRoomCreate = () => {
     navigate(ROUTES.nickname);
   };
 
   return { handleRoomCreate };
 };
+
+export default useCreateRoom;
