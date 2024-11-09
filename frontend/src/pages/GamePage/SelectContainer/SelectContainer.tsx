6ef2cd5699a4719d8fe87edf57f06e1d1de98492
rbgksqkr <rbgks1937@naver.com>
2024-11-09 22:16:47 +0900

    refactor: SelectOption 디렉토리 수정 #276

--- frontend/src/pages/GamePage/SelectContainer/SelectContainer.tsx
+++ frontend/src/pages/GamePage/SelectContainer/SelectContainer.tsx
@@ -2,10 +2,10 @@ import { useParams } from 'react-router-dom';
 
 import useSelectOption from './hooks/useSelectOption';
 import { selectContainerLayout, selectSection } from './SelectContainer.styled';
+import SelectOption from './SelectOption/SelectOption';
 import Timer from './Timer/Timer';
 import SelectButton from '../SelectButton/SelectButton';
 
-import SelectOption from '@/components/SelectOption/SelectOption';
 import useBalanceContentQuery from '@/hooks/useBalanceContentQuery';
 
 const SelectContainer = () => {
