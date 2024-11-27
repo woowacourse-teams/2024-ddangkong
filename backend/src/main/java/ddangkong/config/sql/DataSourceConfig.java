02506c3f5242b87fb3b1c2c73d970a25f56acaef
leegwichan <cnddkscndgus@naver.com>
2024-11-27 18:37:09 +0900

    feat: 변경된 인프라에 맞추어 Replication 코드 비활성화

--- backend/src/main/java/ddangkong/config/sql/DataSourceConfig.java
+++ backend/src/main/java/ddangkong/config/sql/DataSourceConfig.java
@@ -12,7 +12,7 @@ import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Profile;
 import org.springframework.jdbc.datasource.LazyConnectionDataSourceProxy;
 
-@Profile("prod")
+@Profile("inactive")
 @Configuration
 public class DataSourceConfig {
 
