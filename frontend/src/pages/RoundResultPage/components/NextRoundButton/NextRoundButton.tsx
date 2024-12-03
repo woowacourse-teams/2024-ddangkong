ddb3bee05fbe879c92578c2448e6c1d99980e9f7
novice0840 <magnolia0840@gmail.com>
2024-12-03 12:56:05 +0900

    refactor: 긴 상대 경로 import 절대 경로로 변경 #404

--- frontend/src/pages/RoundResultPage/components/NextRoundButton/NextRoundButton.tsx
+++ frontend/src/pages/RoundResultPage/components/NextRoundButton/NextRoundButton.tsx
@@ -3,10 +3,10 @@ import { useParams } from 'react-router-dom';
 
 import useMoveNextRoundMutation from './NextRoundButton.hook';
 import { createRandomNextRoundMessage, getNextRoundButtonText } from './NextRoundButton.utils';
-import Button from '../../../../components/common/Button/Button';
-import { bottomButtonLayout } from '../../../../components/common/Button/Button.styled';
 
 import AlertModal from '@/components/AlertModal/AlertModal';
+import Button from '@/components/common/Button/Button';
+import { bottomButtonLayout } from '@/components/common/Button/Button.styled';
 import useBalanceContentQuery from '@/hooks/useBalanceContentQuery';
 import useIsMaster from '@/hooks/useIsMaster';
 import useModal from '@/hooks/useModal';
