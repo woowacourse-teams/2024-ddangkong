9646244868a467500e6aac25849336f78d93db0a
novice0840 <magnolia0840@gmail.com>
2024-12-06 21:04:58 +0900

    chore: eslint plugin 추가 #429

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -1,118 +1,81 @@
-import { fixupConfigRules, fixupPluginRules } from '@eslint/compat';
+import eslint from '@eslint/js';
 import react from 'eslint-plugin-react';
+import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
+import importPlugin from 'eslint-plugin-import';
+import tseslint from 'typescript-eslint';
 import globals from 'globals';
-import path from 'node:path';
-import { fileURLToPath } from 'node:url';
-import js from '@eslint/js';
-import { FlatCompat } from '@eslint/eslintrc';
-import eslintPluginPrettier from 'eslint-plugin-prettier';
+import { fixupPluginRules } from '@eslint/compat';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-  recommendedConfig: js.configs.recommended,
-  allConfig: js.configs.all,
-});
+export default tseslint.config({
+  extends: [
+    eslint.configs.recommended,
+    tseslint.configs.recommended,
+    importPlugin.flatConfigs.recommended,
+    react.configs.flat.recommended,
+  ],
+  files: ['**/*.ts', '**/*.tsx'],
+  languageOptions: {
+    parserOptions: {
+      ecmaFeatures: {
+        jsx: true,
+      },
+    },
+    globals: {
+      ...globals.browser,
+    },
+  },
 
-export default [
-  eslintPluginPrettier,
-  {
-    ignores: [
-      '**/webpack.config.common.js',
-      '**/webpack.config.dev.js',
-      '**/webpack.config.prod.js',
-      '**/jest.polyfills.js',
-      '**/.eslintrc.js',
-      'eslint-ddangkong/*',
+  plugins: {
+    react,
+    'react-hooks': reactHooks,
+    'react-refresh': reactRefresh,
+  },
+  rules: {
+    ...reactHooks.configs.recommended.rules,
+    'no-console': 'error',
+    'react/react-in-jsx-scope': 'off',
+    'react/no-unknown-property': ['error', { ignore: ['css'] }],
+    'react/function-component-definition': [
+      'error',
+      {
+        namedComponents: 'arrow-function',
+      },
+    ],
+    'react-refresh/only-export-components': [
+      'warn',
+      {
+        allowConstantExport: true,
+      },
+    ],
+    'import/no-unresolved': 'off',
+    'import/order': [
+      'error',
+      {
+        groups: ['builtin', 'external', ['parent', 'sibling'], 'index'],
+        pathGroups: [
+          {
+            pattern: 'react*',
+            group: 'builtin',
+            position: 'before',
+          },
+          {
+            pattern: '@/*',
+            group: 'internal',
+            position: 'after',
+          },
+        ],
+        alphabetize: {
+          order: 'asc',
+          caseInsensitive: true,
+        },
+        'newlines-between': 'always',
+      },
     ],
   },
-  ...fixupConfigRules(
-    compat.extends(
-      'eslint:recommended',
-      'plugin:react/recommended',
-      'plugin:react-hooks/recommended',
-      'plugin:import/recommended',
-      'plugin:jsx-a11y/recommended',
-      'plugin:storybook/recommended',
-      'plugin:@tanstack/eslint-plugin-query/recommended',
-    ),
-  ),
-  {
-    plugins: {
-      react: fixupPluginRules(react),
-      'react-refresh': reactRefresh,
-    },
-
-    languageOptions: {
-      globals: {
-        ...globals.browser,
-      },
-      ecmaVersion: 'latest',
-      sourceType: 'module',
-    },
-
-    settings: {
-      react: {
-        version: 'detect',
-      },
-    },
-
-    rules: {
-      'no-console': 'error',
-      'react/react-in-jsx-scope': 'off',
-      '@typescript-eslint/no-unused-vars': 'warn',
-
-      'react/no-unknown-property': [
-        'error',
-        {
-          ignore: ['css'],
-        },
-      ],
-
-      'react/function-component-definition': [
-        'error',
-        {
-          namedComponents: 'arrow-function',
-        },
-      ],
-
-      'react-refresh/only-export-components': [
-        'warn',
-        {
-          allowConstantExport: true,
-        },
-      ],
-
-      'import/no-unresolved': 'off',
-
-      'import/order': [
-        'error',
-        {
-          groups: ['builtin', 'external', ['parent', 'sibling'], 'index'],
-
-          pathGroups: [
-            {
-              pattern: 'react*',
-              group: 'builtin',
-              position: 'before',
-            },
-            {
-              pattern: '@/*',
-              group: 'internal',
-              position: 'after',
-            },
-          ],
-
-          alphabetize: {
-            order: 'asc',
-            caseInsensitive: true,
-          },
-
-          'newlines-between': 'always',
-        },
-      ],
+  settings: {
+    react: {
+      version: 'detect',
     },
   },
-];
+});
