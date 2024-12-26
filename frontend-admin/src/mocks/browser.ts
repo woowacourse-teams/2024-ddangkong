e85b2c9a1939c97be9b84f7a8ce65cbb75c7ed0a
rbgksqkr <rbgks1937@naver.com>
2024-12-26 17:41:39 +0900

    refactor: mocks 핸들러 폴더 구조 수정 #442

--- frontend-admin/src/mocks/browser.ts
+++ frontend-admin/src/mocks/browser.ts
@@ -1,4 +1,4 @@
 import { setupWorker } from "msw/browser";
-import handlers from "./handler";
+import { handlers } from "./handler";
 
 export const worker = setupWorker(...handlers);
--- frontend-admin/src/mocks/handler.ts
+++
@@ -1,9 +0,0 @@
-import { http, HttpResponse } from "msw";
-
-const handlers = [
-  http.get("/api/test", () => {
-    return HttpResponse.json({ id: "id", name: "name" });
-  }),
-];
-
-export default handlers;
