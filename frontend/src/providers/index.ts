9fe8d60f4c7b512033693f8e13f4d7f147b34e3a
novice0840 <magnolia0840@gmail.com>
2024-12-27 01:10:14 +0900

    refactor: provider 폴더 Barrel Pattern 적용 #439

---
+++ frontend/src/providers/index.ts
@@ -0,0 +1,5 @@
+import ModalProvider from './ModalProvider/ModalProvider';
+import QueryClientDefaultOptionProvider from './QueryClientDefaultOptionProvider/QueryClientDefaultOptionProvider';
+import ToastProvider from './ToastProvider/ToastProvider';
+
+export { ModalProvider, QueryClientDefaultOptionProvider, ToastProvider };
