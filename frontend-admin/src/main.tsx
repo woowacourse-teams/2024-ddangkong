08cb16bba994474308d5ac58167db3dceca42618
rbgksqkr <rbgks1937@naver.com>
2024-12-26 15:09:14 +0900

    chore: QueryProvider, RouterProvider 설정 #442

--- frontend-admin/src/main.tsx
+++ frontend-admin/src/main.tsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { RouterProvider } from "react-router-dom";
+import { router } from "./router/index.tsx";
 
-createRoot(document.getElementById('root')!).render(
+const queryClient = new QueryClient();
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
+  </StrictMode>
+);
---
+++ frontend-admin/src/router/index.tsx
@@ -0,0 +1,18 @@
+import ContentPage from "@/pages/ContentPage/ContentPage";
+import LoginPage from "@/pages/LoginPage/LoginPage";
+import { createBrowserRouter, Navigate } from "react-router-dom";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Navigate to="/login" replace />,
+  },
+  {
+    path: "/login",
+    element: <LoginPage />,
+  },
+  {
+    path: "/content",
+    element: <ContentPage />,
+  },
+]);
