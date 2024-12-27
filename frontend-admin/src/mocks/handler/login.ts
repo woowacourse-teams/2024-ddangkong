34327e20a087789e70e5c386df34a739ce47b958
rbgksqkr <rbgks1937@naver.com>
2024-12-28 02:03:35 +0900

    refactor: handler 반환값 수정 #448

--- frontend-admin/src/mocks/handler/login.ts
+++ frontend-admin/src/mocks/handler/login.ts
@@ -3,11 +3,11 @@ import { http, HttpResponse } from "msw";
 import { MOCK_API_URL } from "@/constants/url";
 
 const loginHandler = () => {
-  return new HttpResponse(null, { status: 200 });
+  return HttpResponse.json({ status: 200 });
 };
 
 const logoutHandler = () => {
-  return new HttpResponse(null, { status: 200 });
+  return HttpResponse.json({ status: 200 });
 };
 
 export const loginHandlers = [
