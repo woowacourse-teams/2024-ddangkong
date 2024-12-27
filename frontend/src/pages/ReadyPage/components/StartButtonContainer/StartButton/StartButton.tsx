e450097e4a5947a612f79cf5494cbb0a0c2d269d
novice0840 <magnolia0840@gmail.com>
2024-12-27 18:58:51 +0900

    feat: collectGameMemberNumberGA 함수 연동 #430

--- frontend/src/pages/ReadyPage/components/StartButtonContainer/StartButton/StartButton.tsx
+++ frontend/src/pages/ReadyPage/components/StartButtonContainer/StartButton/StartButton.tsx
@@ -2,15 +2,23 @@ import useGameStart from './hooks/useGameStart';
 import getStartButtonText from './StartButton.utils';
 
 import Button from '@/components/common/Button/Button';
+import { useGetRoomInfo } from '@/hooks/useGetRoomInfo';
+import { collectGameMemberNumberGA } from '@/lib/googleAnalytics/room';
 
 const StartButton = () => {
   const { isMaster, handleGameStart, isPending, isSuccess } = useGameStart();
+  const { members } = useGetRoomInfo();
+
+  const handleClick = () => {
+    handleGameStart();
+    collectGameMemberNumberGA(members.length);
+  };
 
   return (
     <Button
       text={getStartButtonText(isMaster, isPending || isSuccess)}
       disabled={!isMaster || isPending || isSuccess}
-      onClick={handleGameStart}
+      onClick={handleClick}
       bottom
     />
   );
