d3b666e4f6a3a75ab35fedd5dff78de134b98547
rbgksqkr <rbgks1937@naver.com>
2025-01-02 01:20:03 +0900

    refactor: login API 명세 변경 #448

--- frontend-admin/src/apis/login.ts
+++ frontend-admin/src/apis/login.ts
@@ -2,13 +2,15 @@ import { API_URL } from '@/constants/url';
 import fetcher from './fetcher';
 
 interface LoginParams {
+  nickname: string;
   password: string;
 }
 
 // 로그인
-export const login = async ({ password }: LoginParams) => {
+export const login = async ({ nickname, password }: LoginParams) => {
   return await fetcher.post(API_URL.login, {
     body: {
+      nickname,
       password,
     },
     auth: false,
