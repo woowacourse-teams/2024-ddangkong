ce4cfc80dded4add5440eb77b3713d1be8b92254
rbgksqkr <rbgks1937@naver.com>
2025-01-02 02:07:50 +0900

    refactor: 로그인 페이지에 닉네임 입력 필드 추가 #448

--- frontend-admin/src/pages/LoginPage/LoginPage.tsx
+++ frontend-admin/src/pages/LoginPage/LoginPage.tsx
@@ -1,18 +1,34 @@
 import SpinDdangkong from '@/assets/images/spinDdangkong.webp';
 import {
-  errorMessage,
   image,
   loginButton,
   loginContainer,
   loginHeaderContainer,
   loginLayout,
   loginTitle,
-  passwordInput,
 } from './LoginPage.styles';
 import usePassword from './hooks/usePassword';
+import useNickname from './useNickname';
+import useHandleLogin from './useHandleLogin';
+import Input from '@/components/Input/Input';
 
 const LoginPage = () => {
-  const { password, error, handleChange, handleLogin, handleKeyDown } = usePassword();
+  const { nickname, nicknameError, handleChangeNickname, clearNickname, handleNicknameError } =
+    useNickname();
+  const { password, passwordError, handleChangePassword, clearPassword, handlePasswordError } =
+    usePassword();
+  const { handleLogin, handleKeyDown } = useHandleLogin({
+    nickname,
+    password,
+    onSuccess: () => {
+      clearNickname();
+      clearPassword();
+    },
+    onError: () => {
+      handleNicknameError();
+      handlePasswordError();
+    },
+  });
 
   return (
     <div css={loginLayout}>
@@ -21,16 +37,24 @@ const LoginPage = () => {
           <img src={SpinDdangkong} alt="땅콩 로고" css={image} />
           <span css={loginTitle}>ddangkong</span>
         </div>
-        <div>
-          <input
-            css={passwordInput}
-            value={password}
-            onChange={handleChange}
-            onKeyDown={handleKeyDown}
-            placeholder="비밀번호를 입력해주세요"
-          />
-          <div css={errorMessage(Boolean(error))}>{error || ' '}</div>
-        </div>
+        <Input
+          label="닉네임"
+          value={nickname}
+          handleChange={handleChangeNickname}
+          handleKeyDown={handleKeyDown}
+          hasError={Boolean(nicknameError)}
+          error={nicknameError}
+          placeholder="닉네임을 입력해주세요"
+        />
+        <Input
+          label="비밀번호"
+          value={password}
+          handleChange={handleChangePassword}
+          handleKeyDown={handleKeyDown}
+          hasError={Boolean(passwordError)}
+          error={passwordError}
+          placeholder="비밀번호를 입력해주세요"
+        />
         <button css={loginButton} onClick={handleLogin}>
           로그인
         </button>
