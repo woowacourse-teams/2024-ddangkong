09fbd570ca94ba54c397f17cadffb639adcdfcf5
jhon3242 <rldotnr2@gmail.com>
2025-01-02 17:45:34 +0900

    feat: 통일성을 위해 롬복 추가 #444

--- backend/src/main/java/ddangkong/config/CookieConfig.java
+++ backend/src/main/java/ddangkong/config/CookieConfig.java
@@ -2,18 +2,16 @@ package ddangkong.config;
 
 import ddangkong.resolver.RoomMemberArgumentResolver;
 import java.util.List;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@RequiredArgsConstructor
 public class CookieConfig implements WebMvcConfigurer {
 
     private final RoomMemberArgumentResolver roomMemberArgumentResolver;
-
-    public CookieConfig(RoomMemberArgumentResolver roomMemberArgumentResolver) {
-        this.roomMemberArgumentResolver = roomMemberArgumentResolver;
-    }
 
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
