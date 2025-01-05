9d4b3da7fbb7d858ac93bd8784b72048cdfccb73
leegwichan <cnddkscndgus@naver.com>
2025-01-05 21:03:52 +0900

    refactor: session 에 저장하기 위해 Serializable 을 상속 #451

--- backend/src/main/java/ddangkong/controller/admin/Admin.java
+++ backend/src/main/java/ddangkong/controller/admin/Admin.java
@@ -1,9 +1,10 @@
 package ddangkong.controller.admin;
 
+import java.io.Serializable;
 import lombok.Getter;
 
 @Getter
-public class Admin {
+public class Admin implements Serializable {
 
     private final String nickname;
 
