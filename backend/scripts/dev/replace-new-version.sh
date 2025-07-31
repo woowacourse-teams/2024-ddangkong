3609f4e1bcd0cc9b3913d4bdc6279d18baf04c32
leegwichan <cnddkscndgus@naver.com>
2025-07-31 10:26:52 +0900

    chore: WAS 교체 스크립트 수정

--- backend/scripts/dev/replace-new-version.sh
+++ backend/scripts/dev/replace-new-version.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-PID=$(lsof -t -i:8080)
+PIDS=$(lsof -t -i:8080)
 
 # 프로세스 종료
-if [ -z "$PID" ]; then
+if [ -z "$PIDS" ]; then
   echo "No process is using port 8080."
 else
-  echo "Killing process with PID: $PID"
-  kill -15 "$PID"
+  echo "Killing process with PIDS: $PIDS"
+  kill -15 $PIDS
 
   # 직전 명령(프로세스 종료 명령)이 정상 동작했는지 확인
   if [ $? -eq 0 ]; then
-    echo "Process $PID terminated successfully."
+    echo "Process $PIDS terminated successfully."
   else
-    echo "Failed to terminate process $PID."
+    echo "Failed to terminate process $PIDS."
   fi
 fi
 
