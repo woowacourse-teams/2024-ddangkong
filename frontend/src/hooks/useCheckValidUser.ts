db43778fd3d1a11e28479345a9711efd5153df91
novice0840 <magnolia0840@gmail.com>
2025-07-31 20:15:26 +0900

    fix: useCheckValidUser 경로 수정 #487

--- frontend/src/hooks/useCheckValidUser.ts
+++ frontend/src/hooks/useCheckValidUser.ts
@@ -4,6 +4,7 @@ import { useLocation, useNavigate, useParams } from 'react-router-dom';
 
 import { getUserInfo } from '@/apis/room';
 import { QUERY_KEYS } from '@/constants/queryKeys';
+import { ROUTES } from '@/constants/routes';
 
 const USER_INFO_STALE_TIME = 2 * 60 * 60 * 1000;
 
@@ -11,10 +12,15 @@ const useCheckValidUser = () => {
   const location = useLocation();
   const { roomId } = useParams();
   const navigate = useNavigate();
+
+  const shouldSkipQuery =
+    location.pathname === ROUTES.main || location.pathname === ROUTES.nickname;
+
   const { data } = useQuery({
     queryKey: [QUERY_KEYS.getUserInfo, roomId],
     queryFn: getUserInfo,
     staleTime: USER_INFO_STALE_TIME,
+    enabled: !shouldSkipQuery,
   });
 
   useEffect(() => {
