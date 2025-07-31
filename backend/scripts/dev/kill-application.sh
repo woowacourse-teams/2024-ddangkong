6e7d25f15194ea84a916d62a79ea1297f13508be
leegwichan <cnddkscndgus@naver.com>
2025-07-31 10:10:10 +0900

    chore: 프로세스 종료 스크립트 수정

--- backend/scripts/dev/kill-application.sh
+++ backend/scripts/dev/kill-application.sh
@@ -1,9 +1,9 @@
-PID=$(lsof -t -i:8080)
+PIDS=$(lsof -t -i:8080)
 
 # 프로세스 종료
-if [ -z "$PID" ]; then
+if [ -z "$PIDS" ]; then
   echo "No process is using port 8080."
 else
-  sudo kill -15 "$PID"
-  echo "Killing process with PID: $PID"
+  sudo kill -15 $PIDS
+  echo "Killing process with PIDS: $PIDS"
 fi
