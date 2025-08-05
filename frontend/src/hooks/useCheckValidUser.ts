6e49df65e8c1e5014cd952ee238283f55931fb69
novice0840 <magnolia0840@gmail.com>
2025-08-05 18:15:19 +0900

    refactor: useCheckValidUser hook 설명 추가 #487

--- frontend/src/hooks/useCheckValidUser.ts
+++ frontend/src/hooks/useCheckValidUser.ts
@@ -8,19 +8,20 @@ import { ROUTES } from '@/constants/routes';
 
 const USER_INFO_STALE_TIME = 2 * 60 * 60 * 1000;
 
+// 게임 중 잘못된 유저가 들어오는 것을 방지하기 위한 hook
 const useCheckValidUser = () => {
   const location = useLocation();
   const { roomId } = useParams();
   const navigate = useNavigate();
 
-  const shouldSkipQuery =
+  const NOT_IN_GAME_PATH =
     location.pathname === ROUTES.main || location.pathname === ROUTES.nickname;
 
   const { data } = useQuery({
     queryKey: [QUERY_KEYS.getUserInfo, roomId],
     queryFn: getUserInfo,
     staleTime: USER_INFO_STALE_TIME,
-    enabled: !shouldSkipQuery,
+    enabled: !NOT_IN_GAME_PATH,
   });
 
   useEffect(() => {
