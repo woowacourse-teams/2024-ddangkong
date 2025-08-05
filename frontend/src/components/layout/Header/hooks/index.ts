a75fdcf266a712f53a1e87c6acc545b2d4b8cb12
useon <roqkftjs@gmail.com>
2025-08-05 20:56:34 +0900

    feat: 게임 중단 mutation 훅 구현 #483

--- frontend/src/components/layout/Header/hooks/index.ts
+++ frontend/src/components/layout/Header/hooks/index.ts
@@ -1,5 +1,6 @@
 import useBlockRefresh from './useBlockRefresh';
 import useExit from './useExit';
 import useRoutePath from './useRoutePath';
+import useStopGameMutation from './useStopGameMutation';
 
-export { useBlockRefresh, useExit, useRoutePath };
+export { useBlockRefresh, useExit, useRoutePath, useStopGameMutation };
