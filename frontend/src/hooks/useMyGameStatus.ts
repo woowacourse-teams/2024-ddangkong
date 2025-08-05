525c1ae7249f0ca531eb6f7bb9ecccc0ccb30bef
useon <roqkftjs@gmail.com>
2025-08-05 21:36:24 +0900

    feat: 게임 중단 버튼 클릭시 게임 결과 화면으로 이동하는 기능 #483

--- frontend/src/hooks/useMyGameStatus.ts
+++ frontend/src/hooks/useMyGameStatus.ts
@@ -20,11 +20,20 @@ const useMyGameStatus = ({ roomId }: UseMyGameStatusProps) => {
   });
 
   useEffect(() => {
-    if (isGameFinished) {
+    // 게임 중단
+    if (isGameFinished && isRoundFinished) {
       navigate(ROUTES.gameResult(Number(roomId)));
+      return;
     }
+    // 라운드 종료
     if (isRoundFinished) {
       navigate(ROUTES.game(Number(roomId)));
+      return;
+    }
+    // 게임 종료
+    if (isGameFinished) {
+      navigate(ROUTES.gameResult(Number(roomId)));
+      return;
     }
   }, [isRoundFinished, isGameFinished, navigate, roomId]);
 };
