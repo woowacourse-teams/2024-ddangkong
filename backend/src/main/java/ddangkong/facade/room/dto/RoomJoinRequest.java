54f22393854c3a31ffe4a4b5c6c55300601a2278
leegwichan <cnddkscndgus@naver.com>
2025-08-10 14:28:03 +0900

    refactor: `imageUrl` 기본 값 제거 #492

--- backend/src/main/java/ddangkong/facade/room/dto/RoomJoinRequest.java
+++ backend/src/main/java/ddangkong/facade/room/dto/RoomJoinRequest.java
@@ -6,14 +6,7 @@ public record RoomJoinRequest(
         @NotBlank
         String nickname,
 
+        @NotBlank
         String imageUrl
 ) {
-
-    @Override
-    public String imageUrl() { // TODO : FE 작업 끝난 후 삭제 예정
-        if (imageUrl == null || imageUrl.isBlank()) {
-            return "https://velog.velcdn.com/images/gwichanlee/post/dfdaead8-d98a-4d42-a975-2fb3edd7dde6/image.png";
-        }
-        return imageUrl;
-    }
 }
